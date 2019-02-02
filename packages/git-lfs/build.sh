0ba956c7b2c65e4ac4beedddae52a1767950150f
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-02-02 16:05:59 +0200

    git-lfs: fix missing help info

--- packages/git-lfs/build.sh
+++ packages/git-lfs/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Git extension for versioning large files"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
 TERMUX_PKG_VERSION=2.6.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/git-lfs/git-lfs/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=e17cd9d4e66d1116be32f7ddc7e660c7f8fabbf510bc01b01ec15a22dd934ead
 
@@ -14,9 +15,16 @@ termux_step_make() {
 	ln -sf $TERMUX_PKG_SRCDIR $GOPATH/github.com/git-lfs/git-lfs
 
 	cd $GOPATH/github.com/git-lfs/git-lfs
+	GOOS=linux GOARCH=amd64 CC=gcc LD=gcc go generate github.com/git-lfs/git-lfs/commands
 	go build git-lfs.go
 }
 
 termux_step_make_install() {
 	install -Dm700 $GOPATH/github.com/git-lfs/git-lfs/git-lfs $TERMUX_PREFIX/bin/git-lfs
+}
+
+termux_step_post_make_install() {
+	## Remove read-only files generated in build process.
+	chmod -R 700 $TERMUX_PKG_BUILDDIR/pkg
+	rm -rf $TERMUX_PKG_BUILDDIR/pkg
 }
