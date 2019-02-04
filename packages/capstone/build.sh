7c789c6d84a695e4de69a47e0ebd659c088b80fe
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-02-05 00:43:37 +0200

    new package: capstone

---
+++ packages/capstone/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=http://www.capstone-engine.org/
+TERMUX_PKG_DESCRIPTION="Disassembly framework"
+TERMUX_PKG_LICENSE="BSD"
+TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
+TERMUX_PKG_VERSION=4.0.1
+TERMUX_PKG_SRCURL=https://github.com/aquynh/capstone/archive/$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=79bbea8dbe466bd7d051e037db5961fdb34f67c9fac5c3471dd105cfb1e05dc7
