5f6a25574e55e9ea5d903fae5f98435d08f3efd5
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-02-05 16:22:18 +0200

    new package: samefile

---
+++ packages/samefile/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=http://www.schweikhardt.net/samefile/
+TERMUX_PKG_DESCRIPTION="Finds files with identical contents, independent of file name"
+TERMUX_PKG_LICENSE="BSD"
+TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
+TERMUX_PKG_VERSION=2.14
+TERMUX_PKG_SRCURL=http://www.schweikhardt.net/samefile-$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=45eabde7a8c20cdc3a59d56c72e29acd35f3c77c6d3bb1d1ca10ea202869e599
+TERMUX_PKG_BUILD_IN_SRC=true
