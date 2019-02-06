d18e4b668c920bc0df50dc1f35ea804193960f4d
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-02-06 20:30:54 +0200

    hashcat: update to 5.1.0
    Note: I was unable to test hashcat v5.1.0 since all my devices
    are unrooted and have Android version >6 and obviously hashcat
    failed.

--- packages/hashcat/build.sh
+++ packages/hashcat/build.sh
@@ -1,19 +1,20 @@
-## I'm unsure that HashCat will work...
-## Though, it worked on Samsung Galaxy S7 with Android 6
-## ROM, but after update to Android 7 and higher it no longer
-## works.
+## COMPLETELY UNTESTED !
+##
+## Previous version (4.0.0) worked on Samsung Galaxy S7
+## with Android 6 ROM. But it is not working on Android 7
+## and higher.
+##
 
 TERMUX_PKG_HOMEPAGE=https://hashcat.net/hashcat
 TERMUX_PKG_DESCRIPTION="World's fastest and most advanced password recovery utility"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
-TERMUX_PKG_VERSION=4.0.0
+TERMUX_PKG_VERSION=5.1.0
 TERMUX_PKG_SRCURL=https://github.com/hashcat/hashcat/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=e14c169d6966830e7b7423e17e678f3333e074ec50dae50bdde40e8e0e8658be
+TERMUX_PKG_SHA256=283beaa68e1eab41de080a58bb92349c8e47a2bb1b93d10f36ea30f418f1e338
+TERMUX_PKG_DEPENDS="libandroid-support"
 TERMUX_PKG_BUILD_IN_SRC=yes
 
-termux_step_post_configure() {
-	mkdir -p $TERMUX_PREFIX/include/CL
-	mv ./cl.h $TERMUX_PREFIX/include/CL/
-	mv ./cl_platform.h $TERMUX_PREFIX/include/CL/
+termux_step_pre_configure() {
+	CFLAGS+=" -isystem $TERMUX_PKG_BUILDER_DIR/include"
 }
