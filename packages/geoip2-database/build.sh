120c2c8c7f69fc8a5c86c243073fd72b6d80b86c
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-02-13 20:43:32 +0200

    geoip2-database: update to 20190212

--- packages/geoip2-database/build.sh
+++ packages/geoip2-database/build.sh
@@ -7,30 +7,18 @@ TERMUX_PKG_BUILD_IN_SRC=true
 
 ## Version is DB modification date. Use script 'check-last-modified.sh'
 ## to view last modification date.
-TERMUX_PKG_VERSION=20190122
+TERMUX_PKG_VERSION=20190212
 
-_TERMUX_PKG_SRCURL=('https://geolite.maxmind.com/download/geoip/database/GeoLite2-City.tar.gz'
+TERMUX_PKG_SRCURL=('https://geolite.maxmind.com/download/geoip/database/GeoLite2-City.tar.gz'
                     'https://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.tar.gz'
                     'https://geolite.maxmind.com/download/geoip/database/GeoLite2-ASN.tar.gz')
 
-_TERMUX_PKG_FILE=('GeoLite2-City.tar.gz'
-                  'GeoLite2-Country.tar.gz'
-                  'GeoLite2-ASN.tar.gz')
-
 ## If these checksums becomes invalid - it's time to update package.
-_TERMUX_PKG_SHA256=('589b8603a6cd98cf134f96de24618ba1d986a954e40409a67f398eb1edbf6084'
-                    '1b627bd7a575500cbf9e675630ab2c9b1632c3727f3eab29a06e50568341bdfa'
-                    '6fc0855cdc3514c5b7cf19fe1a2681c5b1bbc5009c3a89b1453b64c8c235ba2a')
+TERMUX_PKG_SHA256=('faa4d95dacd1236b986abc8d4b7d7aef42c3b4f789d61f93bbbc7605cc656d4b'
+                   '654ac2722dba635161fd789ff96c2685bba20c4fda06902d1687b0606388646e'
+                   '8d888f8f8390f68b5360f0d47440f0b4f62a08228041bc8a7e916a51292f2d03')
 
 termux_step_make_install() {
-	for i in {0..2}; do
-		termux_download "${_TERMUX_PKG_SRCURL[i]}" "${_TERMUX_PKG_FILE[i]}" "${_TERMUX_PKG_SHA256[i]}"
-	done
-
-	for _db in GeoLite2-{City,Country,ASN}; do
-		tar --strip-components=1 -xf $_db.tar.gz --wildcards "*/$_db.mmdb"
-	done
-
-	install -d "${TERMUX_PREFIX}/share/GeoIP"
-	install -m644 -t "${TERMUX_PREFIX}/share/GeoIP" GeoLite2-{City,Country,ASN}.mmdb
+	mkdir -p $TERMUX_PREFIX/share/GeoIP/
+	find . -iname \*.mmdb -exec install -Dm600 '{}' $TERMUX_PREFIX/share/GeoIP/ \;
 }
