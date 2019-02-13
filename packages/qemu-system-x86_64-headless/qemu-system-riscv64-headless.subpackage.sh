c53e9ab215837f0eedd108cdec053b63ac0fc01f
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-02-13 21:50:00 +0200

    add headless variant of QEMU
    Unlike one in x11-packages, it no longer requires X11 deps.
    Useful for those who use cli-only programs.
    
    Usermode emulators are moved here and won't be available in
    x11-packages anymore.

---
+++ packages/qemu-system-x86_64-headless/qemu-system-riscv64-headless.subpackage.sh
@@ -0,0 +1,8 @@
+TERMUX_SUBPKG_INCLUDE="
+bin/qemu-system-riscv64
+share/man/man1/qemu-system-riscv64.1
+"
+
+TERMUX_SUBPKG_DEPENDS="glib, libandroid-shmem, libbz2, libc++, libcurl, libgnutls, libjpeg-turbo, liblzo, libnettle, libpixman, libpng, libsasl, libssh2, libutil, ncurses, qemu-common"
+TERMUX_SUBPKG_DESCRIPTION="A generic and open source machine emulator (riscv64)"
+TERMUX_SUBPKG_CONFLICTS="qemu-system-riscv64"
