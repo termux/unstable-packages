579fbbec86a4dcc82b46822dff12266e2ef621aa
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-02-18 23:54:08 +0200

    restic: move "openssh, rclone" to suggests instead of recommends

--- packages/restic/build.sh
+++ packages/restic/build.sh
@@ -3,10 +3,10 @@ TERMUX_PKG_DESCRIPTION="Fast, secure, efficient backup program"
 TERMUX_PKG_LICENSE="BSD"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
 TERMUX_PKG_VERSION=0.9.4
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://github.com/restic/restic/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=c7dca90fb6fd83cee8b9f6a2776f5839794341af1953d251bf06a91870be7a8e
-TERMUX_PKG_RECOMMENDS="openssh, rclone"
+TERMUX_PKG_SUGGESTS="openssh, rclone"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make() {
