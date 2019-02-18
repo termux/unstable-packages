992b33bc1826367001ebdb3dec24643742035a15
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-02-18 15:36:21 +0200

    coding style: reformat scripts/ci/gitlab.sh

--- scripts/ci/gitlab.sh
+++ scripts/ci/gitlab.sh
@@ -22,39 +22,39 @@
 REPO_DIR=$(realpath "$(dirname "$(realpath "$0")")/../../")
 DEBS_DIR="$REPO_DIR/deb-packages"
 cd "$REPO_DIR" || {
-    echo "[!] Failed to cd into '$REPO_DIR'."
-    exit 1
+	echo "[!] Failed to cd into '$REPO_DIR'."
+	exit 1
 }
 
 ## Create directory where *.deb files will be placed.
 if ! mkdir -p "$DEBS_DIR" > /dev/null 2>&1; then
-    echo "[!] Failed to create directory '$DEBS_DIR'."
-    exit 1
+	echo "[!] Failed to create directory '$DEBS_DIR'."
+	exit 1
 fi
 
 ## Verify that script is running under CI (GitLab).
-if [ -z "${CI_COMMIT_BEFORE_SHA}" ]; then
-    echo "[!] CI_COMMIT_BEFORE_SHA is not set !"
-    exit 1
+if [ -z "$CI_COMMIT_BEFORE_SHA" ]; then
+	echo "[!] CI_COMMIT_BEFORE_SHA is not set !"
+	exit 1
 fi
-if [ -z "${CI_COMMIT_SHA}" ]; then
-    echo "[!] CI_COMMIT_SHA is not set !"
-    exit 1
+if [ -z "$CI_COMMIT_SHA" ]; then
+	echo "[!] CI_COMMIT_SHA is not set !"
+	exit 1
 fi
 
 ## Check for updated files.
 if [ "$CI_COMMIT_BEFORE_SHA" = "0000000000000000000000000000000000000000" ]; then
-    UPDATED_FILES=$(git diff-tree --no-commit-id --name-only -r "${CI_COMMIT_SHA}" | grep -P "packages/")
+	UPDATED_FILES=$(git diff-tree --no-commit-id --name-only -r "$CI_COMMIT_SHA" | grep -P "packages/")
 else
-    UPDATED_FILES=$(git diff-tree --no-commit-id --name-only -r "${CI_COMMIT_BEFORE_SHA}..${CI_COMMIT_SHA}" | grep -P "packages/")
+	UPDATED_FILES=$(git diff-tree --no-commit-id --name-only -r "${CI_COMMIT_BEFORE_SHA}..${CI_COMMIT_SHA}" | grep -P "packages/")
 fi
 
 ## Determine modified packages.
 existing_dirs=""
 for dir in $(echo "$UPDATED_FILES" | grep -oP "packages/[a-z0-9+._-]+" | sort | uniq); do
-    if [ -d "$REPO_DIR/$dir" ]; then
-        existing_dirs+=" $dir"
-    fi
+	if [ -d "$REPO_DIR/$dir" ]; then
+		existing_dirs+=" $dir"
+	fi
 done
 PACKAGE_DIRS="$existing_dirs"
 unset dir existing_dirs
@@ -62,8 +62,8 @@ unset dir existing_dirs
 ## Get names of modified packages.
 PACKAGE_NAMES=$(echo "$PACKAGE_DIRS" | sed 's/packages\///g')
 if [ -z "$PACKAGE_NAMES" ]; then
-    echo "[*] No modified packages found."
-    exit 0
+	echo "[*] No modified packages found."
+	exit 0
 fi
 
 ## Handle arguments.
@@ -71,57 +71,57 @@ fi
 ## It should be either architecture (aarch64, arm, i686, x86_64)
 ## or '--upload'.
 if [ $# -ge 1 ]; then
-    if [ "$1" = "--upload" ]; then
-        exec "$REPO_DIR/scripts/bintray-add-package.sh" --path "$DEBS_DIR" $PACKAGE_NAMES
-    else
-        TERMUX_ARCH="$1"
-        unset BINTRAY_USERNAME
-        unset BINTRAY_API_KEY
-    fi
+	if [ "$1" = "--upload" ]; then
+		exec "$REPO_DIR/scripts/bintray-add-package.sh" --path "$DEBS_DIR" $PACKAGE_NAMES
+	else
+		TERMUX_ARCH="$1"
+		unset BINTRAY_USERNAME
+		unset BINTRAY_API_KEY
+	fi
 else
-    TERMUX_ARCH="aarch64"
+	TERMUX_ARCH="aarch64"
 fi
 
 ## Go to build environment.
 if ! cd "$REPO_DIR/termux-packages" > /dev/null 2>&1; then
-    echo "[!] Failed to cd into '$REPO_DIR/termux-packages'."
-    exit 1
+	echo "[!] Failed to cd into '$REPO_DIR/termux-packages'."
+	exit 1
 fi
 
 echo "[@] Building packages for architecture '$TERMUX_ARCH':"
 build_log="$DEBS_DIR/build-$TERMUX_ARCH.log"
 
 for pkg in $PACKAGE_NAMES; do
-    pkg=$(basename "$pkg")
-    echo "[+]   Processing $pkg:"
+	pkg=$(basename "$pkg")
+	echo "[+]   Processing $pkg:"
 
-    for dep_pkg in $(./scripts/buildorder.py "./packages/$pkg"); do
-        dep_pkg=$(basename "$dep_pkg")
-        echo -n "[+]     Compiling dependency $dep_pkg... "
-        if ./build-package.sh -o "$DEBS_DIR" -a "$TERMUX_ARCH" -s "$dep_pkg" >> "$build_log" 2>&1; then
-            echo "ok"
-        else
-            echo "fail"
-            echo "[=] LAST 1000 LINES OF BUILD LOG:"
-            echo
-            tail -n 1000 "$build_log"
-            echo
-            exit 1
-        fi
-    done
+	for dep_pkg in $(./scripts/buildorder.py "./packages/$pkg"); do
+		dep_pkg=$(basename "$dep_pkg")
+		echo -n "[+]     Compiling dependency $dep_pkg... "
+		if ./build-package.sh -o "$DEBS_DIR" -a "$TERMUX_ARCH" -s "$dep_pkg" >> "$build_log" 2>&1; then
+			echo "ok"
+		else
+			echo "fail"
+			echo "[=] LAST 1000 LINES OF BUILD LOG:"
+			echo
+			tail -n 1000 "$build_log"
+			echo
+			exit 1
+		fi
+	done
 
-    echo -n "[+]     Compiling $pkg... "
-    if ./build-package.sh -f -o "$DEBS_DIR" -a "$TERMUX_ARCH" "$pkg" >> "$build_log" 2>&1; then
-        echo "ok"
-    else
-        echo "fail"
-        echo "[=] LAST 1000 LINES OF BUILD LOG:"
-        echo
-        tail -n 1000 "$build_log"
-        echo
-        exit 1
-    fi
+	echo -n "[+]     Compiling $pkg... "
+	if ./build-package.sh -f -o "$DEBS_DIR" -a "$TERMUX_ARCH" "$pkg" >> "$build_log" 2>&1; then
+		echo "ok"
+	else
+		echo "fail"
+		echo "[=] LAST 1000 LINES OF BUILD LOG:"
+		echo
+		tail -n 1000 "$build_log"
+		echo
+		exit 1
+	fi
 
-    echo "[+]   Successfully built $pkg."
+	echo "[+]   Successfully built $pkg."
 done
 echo "[@] Finished successfully."
