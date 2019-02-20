fa8ae065fff606bf2b14072766968ab82cd689c1
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-02-20 20:43:17 +0200

    gitea: update to 1.7.2

--- packages/gitea/build.sh
+++ packages/gitea/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://gitea.io
 TERMUX_PKG_DESCRIPTION="Git with a cup of tea, painless self-hosted git service"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
-TERMUX_PKG_VERSION=1.7.0
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=1.7.2
 TERMUX_PKG_SRCURL=https://github.com/go-gitea/gitea/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=2fdd4d51b22f40c03e6a55c065dafbcfad16ca14adf4f093ceed89e59480f679
+TERMUX_PKG_SHA256=e4c9a8cfab052588ec6d00c94b04c0c718e23fa66fcd7e70815e13006f14e46f
 TERMUX_PKG_DEPENDS="git"
 
 termux_step_make() {
