18f23677b1da4eae7875e299e3b3042b77d29332
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-02-25 20:40:30 +0200

    iverilog: attempt to fix compilation error

--- packages/iverilog/build.sh
+++ packages/iverilog/build.sh
@@ -5,6 +5,7 @@ TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
 TERMUX_PKG_VERSION=10.2
 TERMUX_PKG_SRCURL=https://github.com/steveicarus/iverilog/archive/v${TERMUX_PKG_VERSION/./_}.tar.gz
 TERMUX_PKG_SHA256=f54d91821223c71c70f4735a1fb2af39c62efbccdeb9b0060ea1cf9c67647ee3
+TERMUX_PKG_DEPENDS="libbz2"
 
 termux_step_pre_configure() {
 	aclocal
