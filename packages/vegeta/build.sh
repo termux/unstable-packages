6228811dd77898c8d52310afb07df276f2709942
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-02-25 19:27:19 +0200

    new package: vegeta

---
+++ packages/vegeta/build.sh
@@ -0,0 +1,25 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/tsenart/vegeta
+TERMUX_PKG_DESCRIPTION="HTTP load testing tool"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
+TERMUX_PKG_VERSION=12.2.1
+TERMUX_PKG_SRCURL=https://github.com/tsenart/vegeta/archive/cli/v$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=9d50b69893a321a7929682247c99af57677203c50d187dbc3fd4c823cc3b3e3f
+
+termux_step_make() {
+	termux_setup_golang
+
+	export GOPATH=$TERMUX_PKG_BUILDDIR
+	mkdir -p "$GOPATH"/src/github.com/tsenart
+	ln -sf "$TERMUX_PKG_SRCDIR" "$GOPATH"/src/github.com/tsenart/vegeta
+
+	cd "$GOPATH"/src/github.com/tsenart/vegeta
+	go get -d -v github.com/tsenart/vegeta
+	go build
+}
+
+termux_step_make_install() {
+	install -Dm700 \
+		"$GOPATH"/src/github.com/tsenart/vegeta/vegeta \
+		"$TERMUX_PREFIX"/bin/
+}
