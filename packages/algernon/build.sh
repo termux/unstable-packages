c91bad0afac31c07cea01dda706cb4769724deff
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-02-25 17:18:48 +0200

    new package: algernon

---
+++ packages/algernon/build.sh
@@ -0,0 +1,32 @@
+TERMUX_PKG_HOMEPAGE=https://algernon.roboticoverlords.org/
+TERMUX_PKG_DESCRIPTION="Small self-contained web server with Lua, Markdown, QUIC, Redis and PostgreSQL support"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
+TERMUX_PKG_VERSION=1.12.3
+TERMUX_PKG_SRCURL=https://github.com/xyproto/algernon/archive/$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=2b771dd9a2770b1695ff5dec021ade397872d6d9c8d4bf14b011fa759476cb72
+TERMUX_PKG_KEEP_SHARE_DOC=true
+
+termux_step_make() {
+	termux_setup_golang
+
+	export GOPATH=$TERMUX_PKG_BUILDDIR
+	mkdir -p "$GOPATH"/src/github.com/xyproto
+	ln -sf "$TERMUX_PKG_SRCDIR" "$GOPATH"/src/github.com/xyproto/algernon
+
+	cd "$GOPATH"/src/github.com/xyproto/algernon
+	#go get -d -v bitbucket.org/tmedwards/tweego
+	go build
+}
+
+termux_step_make_install() {
+	install -Dm700 \
+		"$GOPATH"/src/github.com/xyproto/algernon/algernon \
+		"$TERMUX_PREFIX"/bin/
+
+	# Offline samples may be useful to get started with Algernon.
+	rm -rf "$TERMUX_PREFIX"/share/doc/algernon
+	mkdir -p "$TERMUX_PREFIX"/share/doc/algernon
+	cp -a "$GOPATH"/src/github.com/xyproto/algernon/samples \
+		"$TERMUX_PREFIX"/share/doc/algernon/
+}
