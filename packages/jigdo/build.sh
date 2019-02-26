bf2cf0f1fe0ba0cf75495a0e39295859acc5b36d
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-02-26 23:42:09 +0200

    bump C++ packages

--- packages/jigdo/build.sh
+++ packages/jigdo/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Distribute large images by sending and receiving the fil
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
 TERMUX_PKG_VERSION=0.7.3
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=http://atterer.org/sites/atterer/files/2009-08/jigdo/jigdo-$TERMUX_PKG_VERSION.tar.bz2
 TERMUX_PKG_SHA256=875c069abad67ce67d032a9479228acdb37c8162236c0e768369505f264827f0
 TERMUX_PKG_DEPENDS="libbz2, libc++, libdb, wget"
