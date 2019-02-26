bc674e6d8190dc453a136d5267c3ec0dba7774e7
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-02-26 22:02:39 +0200

    libnfs: update to 4.0.0

--- packages/libnfs/build.sh
+++ packages/libnfs/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/sahlberg/libnfs
 TERMUX_PKG_DESCRIPTION="NFS client library"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
-TERMUX_PKG_VERSION=3.0.0
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=4.0.0
 TERMUX_PKG_SRCURL=https://github.com/sahlberg/libnfs/archive/libnfs-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=445d92c5fc55e4a5b115e358e60486cf8f87ee50e0103d46a02e7fb4618566a5
+TERMUX_PKG_SHA256=6ee77e9fe220e2d3e3b1f53cfea04fb319828cc7dbb97dd9df09e46e901d797d
 
 termux_step_pre_configure() {
 	autoreconf -fi
