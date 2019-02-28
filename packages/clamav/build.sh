8d96b000ea94a6d0dad806f93d2dd70bef53018c
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-02-28 16:01:42 +0200

    clamav: fix configuration files
    * Add configuration file for clamd.
    * Specify configuration files in TERMUX_PKG_CONFFILES.

--- packages/clamav/build.sh
+++ packages/clamav/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Anti-virus toolkit for Unix"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
 TERMUX_PKG_VERSION=0.101.1
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_REVISION=4
 TERMUX_PKG_SRCURL=https://www.clamav.net/downloads/production/clamav-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=fa368fa9b2f57638696150c7d108b06dec284e8d8e3b8e702c784947c01fb806
 TERMUX_PKG_DEPENDS="json-c, libandroid-support, libbz2, libc++, libcurl, libltdl, liblzma, libxml2, openssl, pcre2"
@@ -23,14 +23,21 @@ TERMUX_PKG_RM_AFTER_INSTALL="
 share/man/man5/clamav-milter.conf.5
 share/man/man8/clamav-milter.8"
 
+TERMUX_PKG_CONFFILES="
+etc/clamav/clamd.conf
+etc/clamav/freshclam.conf"
+
 termux_step_pre_configure() {
 	export LIBS="-llog"
 }
 
 termux_step_post_make_install() {
-	install -Dm600 \
-		"$TERMUX_PKG_BUILDER_DIR"/freshclam.conf \
-		"$TERMUX_PREFIX"/etc/clamav/freshclam.conf
+	for conf in clamd.conf freshclam.conf; do
+		install -Dm600 \
+			"$TERMUX_PKG_BUILDER_DIR"/$conf \
+			"$TERMUX_PREFIX"/etc/clamav/$conf
+	done
+	unset conf
 }
 
 termux_step_post_massage() {
