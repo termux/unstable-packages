7d383eb94363140478d35b77c9d0253d5dcfddf5
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-02-28 22:55:22 +0200

    zeronet: few improvements
    * No more patches. This means that ZeroNet can be
      safely updated with interated mechanizm by clicking
      button in web ui.
    
    * No more files left once ZeroNet is uninstalled.

--- packages/zeronet/zeronet.sh
+++ packages/zeronet/zeronet.sh
@@ -1,2 +1,5 @@
 #!@TERMUX_PREFIX@/bin/sh
-exec @TERMUX_PREFIX@/opt/zeronet/zeronet.py "$@"
+exec @TERMUX_PREFIX@/opt/zeronet/zeronet.py \
+	--config_file @TERMUX_PREFIX@/etc/zeronet.conf \
+	--data_dir @TERMUX_PREFIX@/var/lib/zeronet \
+	--log_dir @TERMUX_PREFIX@/var/log/zeronet "$@"
