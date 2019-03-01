404cfb8f57163f3dd4e2dfec8a8658c970443892
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-03-01 02:42:41 +0200

    new package: metasploit

---
+++ packages/metasploit/build.sh
@@ -0,0 +1,46 @@
+TERMUX_PKG_HOMEPAGE=https://www.metasploit.com/
+TERMUX_PKG_DESCRIPTION="Advanced open-source platform for developing, testing, and using exploit code"
+TERMUX_PKG_LICENSE="BSD"
+TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
+TERMUX_PKG_VERSION=4.17.37
+TERMUX_PKG_SRCURL=https://github.com/rapid7/metasploit-framework/archive/$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=50ab4628b9801674dd8a57de431d9d5c68c820a59116d54d8e07067d306b4aa1
+TERMUX_PKG_PLATFORM_INDEPENDENT=true
+
+# Many dependencies specified here are required to build & install
+# ruby gems used by Metasploit.
+TERMUX_PKG_DEPENDS="apr, apr-util, autoconf, bison, clang, coreutils, curl, findutils, git, libffi-dev, libgmp-dev, libgrpc-dev, libpcap-dev, libsqlite-dev, libtool, libxml2-dev, libxslt-dev, make, ncurses, ncurses-dev, ncurses-utils, openssl-dev, pkg-config, postgresql, postgresql-dev, readline-dev, ruby-dev, tar, termux-elf-cleaner, termux-tools, unzip, wget, zip"
+
+termux_step_make_install() {
+	# Metasploit sources.
+	mkdir -p "$TERMUX_PREFIX"/opt
+	rm -rf "$TERMUX_PREFIX"/opt/metasploit-framework
+	cp -a "$TERMUX_PKG_SRCDIR" "$TERMUX_PREFIX"/opt/metasploit-framework
+
+	# Installer.
+	install -Dm700 "$TERMUX_PKG_BUILDER_DIR"/installer.sh \
+		"$TERMUX_PREFIX"/opt/metasploit-framework/installer.sh
+	sed -i "s%\@TERMUX_PREFIX\@%${TERMUX_PREFIX}%g" \
+		"$TERMUX_PREFIX"/opt/metasploit-framework/installer.sh
+
+	# Database configuration file.
+	install -Dm600 "$TERMUX_PKG_BUILDER_DIR"/database.yml \
+		"$TERMUX_PREFIX"/opt/metasploit-framework/config/database.yml
+
+	# Wrapper.
+	install -Dm700 "$TERMUX_PKG_BUILDER_DIR"/msfconsole.sh \
+		"$TERMUX_PREFIX"/bin/msfconsole
+	sed -i "s%\@TERMUX_PREFIX\@%${TERMUX_PREFIX}%g" \
+		"$TERMUX_PREFIX"/bin/msfconsole
+	for i in msfd msfrpc msfrpcd msfvenom; do
+		ln -sfr "$TERMUX_PREFIX"/bin/msfconsole "$TERMUX_PREFIX"/bin/$i
+	done
+}
+
+termux_step_create_debscripts() {
+	{
+		echo "#!$TERMUX_PREFIX/bin/sh"
+		echo "bash $TERMUX_PREFIX/opt/metasploit-framework/installer.sh"
+	} > ./postinst
+	chmod 755 ./postinst
+}
