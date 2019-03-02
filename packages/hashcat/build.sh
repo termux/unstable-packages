284390e245921609003f51ab01418a849e29e986
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-03-02 22:58:12 +0200

    hashcat: make sure that nothing prevents libOpenCL.so from loading
    Now hashcat should support more devices.

--- packages/hashcat/build.sh
+++ packages/hashcat/build.sh
@@ -1,8 +1,5 @@
-## COMPLETELY UNTESTED !
 ##
-## Previous version (4.0.0) worked on Samsung Galaxy S7
-## with Android 6 ROM. But it is not working on Android 7
-## and higher.
+##  Requires OpenCL support in your OS !
 ##
 
 TERMUX_PKG_HOMEPAGE=https://hashcat.net/hashcat
@@ -10,7 +7,7 @@ TERMUX_PKG_DESCRIPTION="World's fastest and most advanced password recovery util
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
 TERMUX_PKG_VERSION=5.1.0
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://github.com/hashcat/hashcat/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=283beaa68e1eab41de080a58bb92349c8e47a2bb1b93d10f36ea30f418f1e338
 TERMUX_PKG_DEPENDS="libandroid-support"
@@ -18,4 +15,15 @@ TERMUX_PKG_BUILD_IN_SRC=yes
 
 termux_step_pre_configure() {
 	CFLAGS+=" -isystem $TERMUX_PKG_BUILDER_DIR/include"
+	LDFLAGS="${LDFLAGS/-landroid-support/} -l$TERMUX_PREFIX/lib/libandroid-support.so"
+}
+
+termux_step_post_make_install() {
+	mkdir -p "$TERMUX_PREFIX"/libexec
+	mv -f "$TERMUX_PREFIX"/bin/hashcat \
+		"$TERMUX_PREFIX"/libexec/
+	install -Dm700 "$TERMUX_PKG_BUILDER_DIR"/hashcat.sh \
+		"$TERMUX_PREFIX"/bin/hashcat
+	sed -i "s%\@TERMUX_PREFIX\@%${TERMUX_PREFIX}%g" \
+		"$TERMUX_PREFIX"/bin/hashcat
 }
