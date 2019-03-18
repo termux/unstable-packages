51f50f481ce920e7e9c7735045c7940927621fdb
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-03-18 14:28:10 +0200

    gopass: update to 1.8.5

--- packages/gopass/build.sh
+++ packages/gopass/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/gopasspw/gopass
 TERMUX_PKG_DESCRIPTION="The slightly more awesome standard unix password manager for teams."
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
-TERMUX_PKG_VERSION=1.8.4
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_VERSION=1.8.5
 TERMUX_PKG_SRCURL=https://github.com/gopasspw/gopass/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=314b317c49ee7f8c59758ab82cf5f6942ce9179c97e53f0f952741dbd0e5471a
+TERMUX_PKG_SHA256=e579c91c8273f9b0f96404f93fc0f77df9bc3ae71e8bbdb86a76fdbea2b87669
 TERMUX_PKG_DEPENDS="git, gnupg"
 TERMUX_PKG_SUGGESTS="termux-api"
 
