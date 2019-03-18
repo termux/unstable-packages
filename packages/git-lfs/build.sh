d7b1b5eb9193f31142ef7610fdf972f05e2c7f0a
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-03-18 14:23:58 +0200

    git-lfs: update to 2.7.1

--- packages/git-lfs/build.sh
+++ packages/git-lfs/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://git-lfs.github.com/
 TERMUX_PKG_DESCRIPTION="Git extension for versioning large files"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
-TERMUX_PKG_VERSION=2.6.1
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=2.7.1
 TERMUX_PKG_SRCURL=https://github.com/git-lfs/git-lfs/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=e17cd9d4e66d1116be32f7ddc7e660c7f8fabbf510bc01b01ec15a22dd934ead
+TERMUX_PKG_SHA256=af60c2370d135ab13724d302a0b1c226ec9fb0ee6d29ecc335e9add4c86497b4
 
 termux_step_make() {
 	termux_setup_golang
