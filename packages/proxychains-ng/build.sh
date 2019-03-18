8fefbcc211509edc8f47972f1f1f7953f539654e
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-03-18 14:23:12 +0200

    proxychains-ng: update to 4.14

--- packages/proxychains-ng/build.sh
+++ packages/proxychains-ng/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/rofl0r/proxychains-ng
 TERMUX_PKG_DESCRIPTION="A hook preloader that allows to redirect TCP traffic of existing dynamically linked programs through one or more SOCKS or HTTP proxies"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
-TERMUX_PKG_VERSION=4.13
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=4.14
 TERMUX_PKG_SRCURL=https://github.com/rofl0r/proxychains-ng/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=ff15295efc227fec99c2b8131ad532e83e833a02470c7a48ae7e7f131e1b08bc
+TERMUX_PKG_SHA256=ab31626af7177cc2669433bb244b99a8f98c08031498233bb3df3bcc9711a9cc
 TERMUX_PKG_BUILD_IN_SRC=yes
 
 termux_step_post_make_install() {
