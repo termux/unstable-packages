16fa741d04a6f4c969bcfd9c94024dc9cba57808
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-04-10 14:37:59 +0300

    google-glog: update to 0.4.0

--- packages/google-glog/build.sh
+++ packages/google-glog/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/google/glog
 TERMUX_PKG_DESCRIPTION="Logging library for C++"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
-TERMUX_PKG_VERSION=0.3.5
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=0.4.0
 TERMUX_PKG_SRCURL=https://github.com/google/glog/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=7580e408a2c0b5a89ca214739978ce6ff480b5e7d8d7698a2aa92fadc484d1e0
+TERMUX_PKG_SHA256=f28359aeba12f30d73d9e4711ef356dc842886968112162bc73002645139c39c
 TERMUX_PKG_DEPENDS="libandroid-glob, libc++"
 
 termux_step_pre_configure() {
