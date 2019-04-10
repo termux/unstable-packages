b75d9e6eb52632736a6054e95e9df176a6df8640
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-04-10 14:38:48 +0300

    direnv: update to 2.20.0

--- packages/direnv/build.sh
+++ packages/direnv/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/direnv/direnv
 TERMUX_PKG_DESCRIPTION="Environment switcher for shell"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
-TERMUX_PKG_VERSION=2.19.2
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=2.20.0
 TERMUX_PKG_SRCURL=https://github.com/direnv/direnv/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=407e46233942277dcac133bcf9dc656c825299c611d0b22b9cd02841c4c34ffb
+TERMUX_PKG_SHA256=cc72525b0a5b3c2ab9a52a3696e95562913cd431f923bcc967591e75b7541bff
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make() {
