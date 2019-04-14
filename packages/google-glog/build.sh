e3d49dd94683c42ff20e3e7bbd3eb4cd85f89fa8
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-04-14 13:27:00 +0300

    google-glog: build shared library instead of static

--- packages/google-glog/build.sh
+++ packages/google-glog/build.sh
@@ -3,10 +3,10 @@ TERMUX_PKG_DESCRIPTION="Logging library for C++"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
 TERMUX_PKG_VERSION=0.4.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/google/glog/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=f28359aeba12f30d73d9e4711ef356dc842886968112162bc73002645139c39c
 TERMUX_PKG_DEPENDS="libandroid-glob, libc++"
 
-termux_step_pre_configure() {
-	export LIBS="-landroid-glob -llog"
-}
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DBUILD_SHARED_LIBS=ON"
