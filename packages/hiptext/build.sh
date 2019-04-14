b84986810761d66e173043ead88c592ac14d7501
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-04-14 14:02:25 +0300

    hiptext: bump revision

--- packages/hiptext/build.sh
+++ packages/hiptext/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://github.com/jart/hiptext
 TERMUX_PKG_DESCRIPTION="Turn images into text better than caca/aalib"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_VERSION=0.2
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/jart/hiptext/releases/download/$TERMUX_PKG_VERSION/hiptext-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=7f2217dec8775b445be6745f7bd439c24ce99c4316a9faf657bee7b42bc72e8f
 TERMUX_PKG_DEPENDS="ffmpeg, freetype, gflags, google-glog, libjpeg-turbo, libpng, ncurses"
