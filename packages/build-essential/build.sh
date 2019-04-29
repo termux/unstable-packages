f8da7836a94a0672bb096d6449cdccd96feb75f8
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-04-30 01:02:31 +0300

    update build-essential

--- packages/build-essential/build.sh
+++ packages/build-essential/build.sh
@@ -2,25 +2,29 @@ TERMUX_PKG_HOMEPAGE=https://github.com/termux/termux-packages
 TERMUX_PKG_DESCRIPTION="A metapackage that installs essential development tools"
 TERMUX_PKG_LICENSE="Public Domain"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
-TERMUX_PKG_VERSION=1.0
+TERMUX_PKG_VERSION=2.0
 TERMUX_PKG_METAPACKAGE=yes
 TERMUX_PKG_PLATFORM_INDEPENDENT=yes
 
-#
-# autoconf, automake, cmake, make, ninja - utilities for common build systems.
-#
-# bc, gperf - required in certain cases.
-#
-# bison, flex - used in complex projects and interpreters.
-#
-# bzip2, coreutils, diffutils, findutils, gawk, grep, gzip, procps, psmisc,
-#
-# git - must have utility for working with Github and other Git hostings.
-#
-# sed, tar - we may need complete versions of the basic utilities instead of ones
-# from busybox.
-#
-TERMUX_PKG_DEPENDS="autoconf, automake, bc, bison, bzip2, clang, cmake, coreutils, diffutils, ed, file, findutils, gawk, gdb, git, gperf, grep, gzip, libtool, m4, make, ninja, patch, pkg-config, procps, psmisc, sed, tar, xz-utils"
+# Common utilities for various build systems.
+_COMMON_DEV_PACKAGES="autoconf, automake, bison, clang, cmake, flex, gperf, libtool, m4, make, ninja, pkg-config"
+
+# These packages replace busybox's applets.
+_CORE_PACKAGES="bc, bzip2, coreutils, diffutils, ed, findutils, gawk, grep, gzip, lzip, lzop, patch, procps, psmisc, sed, tar, xz-utils"
+
+# Useful for debugging.
+_DEBUG_PACKAGES="file, gdb, ltrace, strace"
+
+# Version control systems.
+_VCS_PACKAGES="git, subversion"
+
+# Combine all categories.
+TERMUX_PKG_DEPENDS="${_COMMON_DEV_PACKAGES}, ${_CORE_PACKAGES}, ${_DEBUG_PACKAGES}, ${_VCS_PACKAGES}"
 
 # Other packages that may be interesting.
-TERMUX_PKG_SUGGESTS="cargo, golang, nodejs, python-dev, python2-dev, ruby-dev"
+TERMUX_PKG_SUGGESTS="cargo, golang, nodejs, patchelf, proot, python-dev, python2-dev, ruby-dev"
+
+termux_step_post_massage() {
+	cd "$TERMUX_PKG_MASSAGEDIR"
+	rm -rf data
+}
