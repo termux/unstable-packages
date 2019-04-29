fb7788c77d6a738bc7c71ec4c6937f9e3e5c6a3b
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-04-29 22:12:29 +0300

    new package: hexyl

---
+++ packages/hexyl/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/sharkdp/hexyl
+TERMUX_PKG_DESCRIPTION="A command-line hex viewer"
+TERMUX_PKG_LICENSE="Apache-2.0"
+TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
+TERMUX_PKG_VERSION=0.5.0
+TERMUX_PKG_SRCURL=https://github.com/sharkdp/hexyl/archive/v$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=6241a4cf3e3ec2f32164539ef158c84ad29c53511cd1e3c0148776b8ce5234d4
+TERMUX_PKG_BUILD_IN_SRC=yes
