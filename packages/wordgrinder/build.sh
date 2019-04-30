58dc021bd66d8bd3a9c5c72cc0f0d6080914b854
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-05-01 01:21:21 +0300

    wordgrinder: depend on zlib

--- packages/wordgrinder/build.sh
+++ packages/wordgrinder/build.sh
@@ -3,10 +3,10 @@ TERMUX_PKG_DESCRIPTION="A Unicode-aware character cell word processor"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
 TERMUX_PKG_VERSION=0.7.2
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://github.com/davidgiven/wordgrinder/archive/$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=4e1bc659403f98479fe8619655f901c8c03eb87743374548b4d20a41d31d1dff
-TERMUX_PKG_DEPENDS="liblua, ncurses"
+TERMUX_PKG_DEPENDS="liblua, ncurses, zlib"
 TERMUX_PKG_BUILD_IN_SRC=yes
 
 termux_step_make() {
