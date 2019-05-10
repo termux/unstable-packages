d8cb728e589cec6f57037e65f903b8fb7a910c47
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-05-10 15:41:38 +0300

    lua-lpeg: update to 1.0.2

--- packages/lua-lpeg/build.sh
+++ packages/lua-lpeg/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=http://www.inf.puc-rio.br/~roberto/lpeg
 TERMUX_PKG_DESCRIPTION="Pattern-matching library for Lua 5.3"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
-TERMUX_PKG_VERSION=1.0.1
+TERMUX_PKG_VERSION=1.0.2
 TERMUX_PKG_SRCURL=http://www.inf.puc-rio.br/~roberto/lpeg/lpeg-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=62d9f7a9ea3c1f215c77e0cadd8534c6ad9af0fb711c3f89188a8891c72f026b
+TERMUX_PKG_SHA256=48d66576051b6c78388faad09b70493093264588fcd0f258ddaab1cdd4a15ffe
 TERMUX_PKG_DEPENDS="liblua"
 TERMUX_PKG_BUILD_IN_SRC=true
 
