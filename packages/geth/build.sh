2f9f4c511e8aebf76f6b91e87e2e079d46c818b0
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-05-10 19:07:56 +0300

    geth: bump revision
    Needed for geth-utils subpackage so dependency will be versioned.

--- packages/geth/build.sh
+++ packages/geth/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Go implementation of the Ethereum protocol"
 TERMUX_PKG_LICENSE="LGPL-3.0"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
 TERMUX_PKG_VERSION=1.8.27
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/ethereum/go-ethereum/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=45d264106991d0e2a4c34ac5d6539fc9460c768fc70588ea38a25f467039ece8
 
