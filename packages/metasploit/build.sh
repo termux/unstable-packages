9fd73ef19ae8a999ad454abe670a63cbd76993c0
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-05-23 23:35:49 +0300

    metasploit: update to 5.0.22

--- packages/metasploit/build.sh
+++ packages/metasploit/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.metasploit.com/
 TERMUX_PKG_DESCRIPTION="Advanced open-source platform for developing, testing, and using exploit code"
 TERMUX_PKG_LICENSE="BSD"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
-TERMUX_PKG_VERSION=5.0.19
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=5.0.22
 TERMUX_PKG_SRCURL=https://github.com/rapid7/metasploit-framework/archive/$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=c5a193857fd83f697e8906672fd3f83886f5ddf01704ee49ae843f5820495bba
+TERMUX_PKG_SHA256=9f90dba99cd94397653cbf586389e98cd30c1a0c880e2729a1bfd3b2a3ac2c4d
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
 # Many dependencies specified here are required to build & install
