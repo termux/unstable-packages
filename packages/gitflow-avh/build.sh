357c53686813f0f339a3ff5ea9589a83c678230c
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-05-24 02:11:40 +0300

    gitflow-avh: bump revision
    Needed after https://github.com/termux/termux-packages/pull/3730.

--- packages/gitflow-avh/build.sh
+++ packages/gitflow-avh/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Extend git with Vincent Driessen's branching model. The 
 TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
 TERMUX_PKG_VERSION=1.12.2
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/petervanderdoes/gitflow/archive/$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=9f9345e151fef8fb1049122f9fe5b02af7ec207a2bbfa98700b8940159280c46
 TERMUX_PKG_DEPENDS="dash, git"
