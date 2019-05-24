85c50ed27bace7841832a8a1813338ca9da260d7
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-05-25 00:34:09 +0300

    vegeta: bump revision
    Needed after https://github.com/termux/termux-packages/pull/3730.

--- packages/vegeta/build.sh
+++ packages/vegeta/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="HTTP load testing tool"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
 TERMUX_PKG_VERSION=12.5.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/tsenart/vegeta/archive/cli/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=28b9c30d0dd4af3e83a857194cc8df6d00cb613e3d553c96d151638f509e2820
 
