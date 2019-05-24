bddd021b602e020c7f8aeb6df92734324c46fd38
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-05-25 00:34:10 +0300

    yara: bump revision
    Needed after https://github.com/termux/termux-packages/pull/3730.

--- packages/yara/build.sh
+++ packages/yara/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Tool aimed at helping malware researchers to identify an
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
 TERMUX_PKG_VERSION=3.10.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/VirusTotal/yara/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=3281d43d6b49a4ca8d3a5d2521e06a0b72863702022f981b051856c2b83449c2
 TERMUX_PKG_DEPENDS="file, openssl"
