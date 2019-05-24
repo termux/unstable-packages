e6c3466bb3349a32a7959885b1f5642d8dcedeb4
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-05-24 03:28:17 +0300

    hyperfine: bump revision
    Needed after https://github.com/termux/termux-packages/pull/3730.

--- packages/hyperfine/build.sh
+++ packages/hyperfine/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A command-line benchmarking tool"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
 TERMUX_PKG_VERSION=1.5.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/sharkdp/hyperfine/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=d5183611348e696e579dda846cee92b7b78c8ef18c00bfb9b0a62d0a63034823
 TERMUX_PKG_BUILD_IN_SRC=yes
