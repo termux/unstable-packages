556c320fa90ed205446ad37e4271235dcc18091b
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-05-24 03:28:19 +0300

    libnftnl: bump revision
    Needed after https://github.com/termux/termux-packages/pull/3730.

--- packages/libnftnl/build.sh
+++ packages/libnftnl/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Netfilter library providing interface to the nf_tables s
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
 TERMUX_PKG_VERSION=1.1.2
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=http://netfilter.org/projects/libnftnl/files/libnftnl-$TERMUX_PKG_VERSION.tar.bz2
 TERMUX_PKG_SHA256=a5c7b7a6c13c9c5898b13fcb1126fefce2015d5a96d7c354b19aaa40b6aece5d
 TERMUX_PKG_DEPENDS="libmnl"
