8f14ab1cef171f1e011365185cde166eef710397
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-05-24 03:28:18 +0300

    jp2a: bump revision
    Needed after https://github.com/termux/termux-packages/pull/3730.

--- packages/jp2a/build.sh
+++ packages/jp2a/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
 _COMMIT=61d205f6959d88e0cc8d8879fe7d66eb0932ecca
 TERMUX_PKG_VERSION=1.0.8
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/cslarsen/jp2a/archive/${_COMMIT}.zip
 TERMUX_PKG_SHA256=26f655e4b977bf24b8193150f34c015195c07c1116d064375223dd46ea5b8b4e
 TERMUX_PKG_DEPENDS="libcurl, libjpeg-turbo, ncurses"
