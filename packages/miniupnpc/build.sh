3446916a7fa42b51268b3369e591502cbd544c51
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-05-24 03:28:19 +0300

    miniupnpc: bump revision
    Needed after https://github.com/termux/termux-packages/pull/3730.

--- packages/miniupnpc/build.sh
+++ packages/miniupnpc/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Small UPnP client library and tool to access Internet Ga
 TERMUX_PKG_LICENSE="BSD"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
 TERMUX_PKG_VERSION=2.1.20190408
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://miniupnp.tuxfamily.org/files/miniupnpc-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=a0c46bcf6065d6351a8fa6a0a18dc57d10a16908dbb470908fd2e423511514ec
 
