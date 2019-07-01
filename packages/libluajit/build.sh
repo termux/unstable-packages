4c67d565e073e9ecf4131e7117d1c7bc4ab8a81b
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-07-01 21:35:27 +0300

    libluajit: fix CFLAGS handling
    Prevent usage of $CFLAGS by host compiler by unsetting existing CFLAGS and
    CPPFLAGS variables. All necessary flags already passed through $TARGET_FLAGS.

--- packages/libluajit/build.sh
+++ packages/libluajit/build.sh
@@ -5,7 +5,7 @@ TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com"
 TERMUX_PKG_VERSION=2.1.0~beta3
 TERMUX_PKG_SRCURL=https://github.com/LuaJIT/LuaJIT/archive/v2.1.0-beta3.tar.gz
 TERMUX_PKG_SHA256=409f7fe570d3c16558e594421c47bdd130238323c9d6fd6c83dedd2aaeb082a8
-TERMUX_PKG_EXTRA_MAKE_ARGS="V=1 amalg PREFIX=$TERMUX_PREFIX"
+TERMUX_PKG_EXTRA_MAKE_ARGS="amalg PREFIX=$TERMUX_PREFIX"
 TERMUX_PKG_BUILD_IN_SRC=yes
 
 termux_step_pre_configure() {
@@ -23,10 +23,7 @@ termux_step_pre_configure() {
 	fi
 	export TARGET_FLAGS="$CFLAGS $CPPFLAGS $LDFLAGS"
 	export TARGET_SYS=Linux
-
-	# HOST_CC uses same flags as cross-compiler, but may not accept
-	# the '-Oz' optimization flag.	
-	CFLAGS=${CFLAGS/-Oz/-Os}
+	unset CFLAGS LDFLAGS
 }
 
 termux_step_make_install () {
