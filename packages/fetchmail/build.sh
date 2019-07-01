d0c449754063df7f3f9118e4f497bce779beb17e
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-07-01 23:36:34 +0300

    fetchmail: more python => python2 fixes

--- packages/fetchmail/build.sh
+++ packages/fetchmail/build.sh
@@ -1,9 +1,13 @@
+##
+## NOTE: script 'fetchmailconf' uses TkInter which is not available
+##       in Termux variant of python2.
+##
 TERMUX_PKG_HOMEPAGE=http://www.fetchmail.info/
 TERMUX_PKG_DESCRIPTION="A remote-mail retrieval utility"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
 TERMUX_PKG_VERSION=6.3.26
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://sourceforge.net/projects/fetchmail/files/branch_6.3/fetchmail-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_SHA256=79b4c54cdbaf02c1a9a691d9948fcb1a77a1591a813e904283a8b614b757e850
 TERMUX_PKG_DEPENDS="libcrypt, openssl"
