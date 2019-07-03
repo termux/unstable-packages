469f793fc875ebeaae3c98cbf838bed1fe90114e
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-07-03 22:56:31 +0300

    new package: libburn

---
+++ packages/libburn/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=https://dev.lovelyhq.com/libburnia
+TERMUX_PKG_DESCRIPTION="Library for reading, mastering and writing optical discs"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
+TERMUX_PKG_VERSION=1.5.0
+TERMUX_PKG_SRCURL=http://files.libburnia-project.org/releases/libburn-$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=582b12c236c1365211946f2fe3c254976af37bbec244051f7742a98ded9be2bd
