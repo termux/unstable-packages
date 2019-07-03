dd0efd68834798de056c6fbd3f214cd9b10e6298
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-07-03 23:36:13 +0300

    new package: libisoburn
    Requested in https://github.com/termux/unstable-packages/issues/113.

---
+++ packages/libisoburn/build.sh
@@ -0,0 +1,12 @@
+TERMUX_PKG_HOMEPAGE=https://dev.lovelyhq.com/libburnia
+TERMUX_PKG_DESCRIPTION="Frontend for libraries libburn and libisofs"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
+TERMUX_PKG_VERSION=1.5.0
+TERMUX_PKG_SRCURL=http://files.libburnia-project.org/releases/libisoburn-$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=dac1f5629c404df236453b2b6a2671473900776c6a5707e5b8acae20385c1de5
+TERMUX_PKG_DEPENDS="libburn, libisofs, readline"
+TERMUX_PKG_CONFLICTS="xorriso"
+
+# We don't have tk.
+TERMUX_PKG_RM_AFTER_INSTALL="bin/xorriso-tcltk"
