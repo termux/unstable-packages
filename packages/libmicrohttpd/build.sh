c2cb169f9d20ecd86664dcb630e86088b5aa1b8a
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-07-18 02:05:37 +0300

    libmicrohttpd: update to 0.9.65

--- packages/libmicrohttpd/build.sh
+++ packages/libmicrohttpd/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=http://www.gnu.org/software/libmicrohttpd/
 TERMUX_PKG_DESCRIPTION="A small C library that is supposed to make it easy to run an HTTP server as part of another application"
 TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
-TERMUX_PKG_VERSION=0.9.63
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=0.9.65
 TERMUX_PKG_SRCURL=https://ftp.gnu.org/gnu/libmicrohttpd/libmicrohttpd-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=37c36f1be177f0e37ef181a645cd3baac1000bd322a01c2eff70f3cc8c91749c
+TERMUX_PKG_SHA256=f2467959c5dd5f7fdf8da8d260286e7be914d18c99b898e22a70dafd2237b3c9
 TERMUX_PKG_DEPENDS="libgnutls"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
