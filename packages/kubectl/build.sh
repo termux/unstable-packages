3f32ba777c1afba34ab9f5ba624bbf13ec8259c1
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-07-23 01:14:56 +0300

    kubectl: update to 1.15.1

--- packages/kubectl/build.sh
+++ packages/kubectl/build.sh
@@ -2,14 +2,14 @@ TERMUX_PKG_HOMEPAGE=http://kubernetes.io
 TERMUX_PKG_DESCRIPTION="Kubernetes.io client binary"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
-TERMUX_PKG_VERSION=1.15.0
+TERMUX_PKG_VERSION=1.15.1
 TERMUX_PKG_SRCURL=https://dl.k8s.io/v$TERMUX_PKG_VERSION/kubernetes-src.tar.gz
-TERMUX_PKG_SHA256=4bc800eccf1e788d9a3e426613ba8e04dd2245fb200135845783cf7400d98b38
+TERMUX_PKG_SHA256=afb7e957eed688098888de356b6725a7d6e75e670c36470dd989b4c0140e835d
 
 termux_step_extract_package() {
 	mkdir -p "$TERMUX_PKG_CACHEDIR"
 	mkdir -p "$TERMUX_PKG_SRCDIR"
-	
+
 	termux_download "$TERMUX_PKG_SRCURL" "$TERMUX_PKG_CACHEDIR"/kubernetes-src.tar.gz \
 		"$TERMUX_PKG_SHA256"
 
