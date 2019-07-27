d31d57f2343852212d2163bcbc7bac0ba26fc752
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-07-28 02:59:54 +0300

    bump libjpeg-turbo using packages

--- packages/mupdf/build.sh
+++ packages/mupdf/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Lightweight PDF and XPS viewer (library only)"
 TERMUX_PKG_LICENSE="AGPL-V3"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
 TERMUX_PKG_VERSION=1.15.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://mupdf.com/downloads/archive/mupdf-${TERMUX_PKG_VERSION}-source.tar.xz
 TERMUX_PKG_SHA256=565036cf7f140139c3033f0934b72e1885ac7e881994b7919e15d7bee3f8ac4e
 TERMUX_PKG_DEPENDS="freetype, harfbuzz, jbig2dec, libjpeg-turbo, openjpeg, zlib"
