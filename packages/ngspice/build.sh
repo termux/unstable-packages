eedc84c4645b8bbd6621afc822646c0167a9713f
kenneth-Q <11911577+kenneth-Q@users.noreply.github.com>
2019-08-17 20:00:40 +0800

    Add package: ngspice

---
+++ packages/ngspice/build.sh
@@ -0,0 +1,12 @@
+TERMUX_PKG_HOMEPAGE=http://ngspice.sourceforge.net/
+TERMUX_PKG_DESCRIPTION="open source spice simulator for electric and electronic circuits"
+TERMUX_PKG_LICENSE="BSD"
+TERMUX_PKG_MAINTAINER="mmp developer <mmpdover@gmail.com>"
+TERMUX_PKG_VERSION=30
+TERMUX_PKG_REVISION=1
+TERMUX_PKG_SRCURL=https://nchc.dl.sourceforge.net/project/ngspice/ng-spice-rework/30/ngspice-30.tar.gz
+TERMUX_PKG_SHA256=08fe0e2f3768059411328a33e736df441d7e6e7304f8dad0ed5f28e15d936097
+TERMUX_PKG_DEPENDS="ncurses, fftw, texinfo, libedit, tcl"
+TERMUX_PKG_RECOMMENDS="gnuplot"
+TERMUX_PKG_BUILD_IN_SRC=yes
+
