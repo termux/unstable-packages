c32dbdcbcad0ce58d9e61e29c3cbbedb6e712c56
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-08-18 20:29:40 +0300

    qemu: update dependencies for user target subpackages

--- packages/qemu-common/qemu-user-riscv64.subpackage.sh
+++ packages/qemu-common/qemu-user-riscv64.subpackage.sh
@@ -1,3 +1,3 @@
 TERMUX_SUBPKG_DESCRIPTION="CPU usermode emulation (riscv64)"
-TERMUX_SUBPKG_DEPENDS="capstone, glib, libandroid-shmem, libc++"
+TERMUX_SUBPKG_DEPEND_ON_PARENT="deps"
 TERMUX_SUBPKG_INCLUDE="bin/qemu-riscv64"
--- packages/qemu-common/qemu-user-x86_64.subpackage.sh
+++ packages/qemu-common/qemu-user-x86_64.subpackage.sh
@@ -1,3 +1,3 @@
 TERMUX_SUBPKG_DESCRIPTION="CPU usermode emulation (x86_64)"
-TERMUX_SUBPKG_DEPENDS="capstone, glib, libandroid-shmem, libc++"
+TERMUX_SUBPKG_DEPEND_ON_PARENT="deps"
 TERMUX_SUBPKG_INCLUDE="bin/qemu-x86_64"
