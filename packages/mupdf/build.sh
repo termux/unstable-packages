1aa99ab9fa1526ea6c0bf8a5f551ce1a9f30978c
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-08-20 23:07:13 +0300

    mupdf: update to 1.16.1

--- packages/mupdf/build.sh
+++ packages/mupdf/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://mupdf.com/
 TERMUX_PKG_DESCRIPTION="Lightweight PDF and XPS viewer (library only)"
 TERMUX_PKG_LICENSE="AGPL-V3"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
-TERMUX_PKG_VERSION=1.15.0
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=1.16.1
 TERMUX_PKG_SRCURL=https://mupdf.com/downloads/archive/mupdf-${TERMUX_PKG_VERSION}-source.tar.xz
-TERMUX_PKG_SHA256=565036cf7f140139c3033f0934b72e1885ac7e881994b7919e15d7bee3f8ac4e
+TERMUX_PKG_SHA256=6fe78184bd5208f9595e4d7f92bc8df50af30fbe8e2c1298b581c84945f2f5da
 TERMUX_PKG_DEPENDS="freetype, harfbuzz, jbig2dec, libjpeg-turbo, openjpeg, zlib"
 TERMUX_PKG_EXTRA_MAKE_ARGS="prefix=$TERMUX_PREFIX build=release libs"
 TERMUX_PKG_BUILD_IN_SRC=true
