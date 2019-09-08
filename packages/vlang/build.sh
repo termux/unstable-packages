ac31a82b541e5457373001c80d14b619ba5dc416
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-09-09 01:24:36 +0300

    vlang: update to 0.1.18

--- packages/vlang/build.sh
+++ packages/vlang/build.sh
@@ -2,23 +2,26 @@ TERMUX_PKG_HOMEPAGE=https://github.com/vlang/v
 TERMUX_PKG_DESCRIPTION="Simple, fast, safe, compiled language for developing maintainable software"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
-_COMMIT=7a3b5c20dd549f2f6022acfcefc376aeef9db903
-TERMUX_PKG_VERSION=0.1.11-g${_COMMIT:0:6}
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://github.com/vlang/v/archive/${_COMMIT}.zip
-TERMUX_PKG_SHA256=30cb901b969612c231e9f81b803c184bd05ac94975c61d000ba947564b8661ff
+TERMUX_PKG_VERSION=0.1.18
+TERMUX_PKG_SRCURL=("https://github.com/vlang/v/archive/$TERMUX_PKG_VERSION.tar.gz"
+                   "https://github.com/vlang/vc/archive/$TERMUX_PKG_VERSION.tar.gz")
+TERMUX_PKG_SHA256=("3f3407a78aca7fc3b42a3fc1f1d2b9724c1e4c71fbd5d37ff12976cd2305cec1"
+                   "b8697082b4b89aeadae2a5bf3626459419f3826b061ecf960fc582c854361e2e")
 TERMUX_PKG_DEPENDS="clang"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make() {
-	export VROOT="$HOME/.vlang"
-
 	# Vlang doesn't seems to support cross-compilation so we need only to obtain
 	# the generated C source file and compile it for target.
-	rm -rf "$VROOT"
-	make
+	(unset CC CFLAGS CPPFLAGS LDFLAGS;
+		gcc "vc-$TERMUX_PKG_VERSION"/v.c -o v.host -lm
+		./v.host -debug -o v.host.2 compiler
+		mv .v.host.2.c v.c
+	)
+
+	$CC $CFLAGS $CPPFLAGS v.c -o v -lm
 }
 
 termux_step_make_install() {
-	$CC $CFLAGS $CPPFLAGS "$VROOT"/v.c -o "$TERMUX_PREFIX"/bin/v
+	install -Dm700 v "$TERMUX_PREFIX"/bin/v
 }
