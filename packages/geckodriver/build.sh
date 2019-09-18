0b7f921dc1bb075fb6025668e6cbe6c26decd0bf
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-09-18 21:05:47 +0300

    geckodriver: update to 0.25.0

--- packages/geckodriver/build.sh
+++ packages/geckodriver/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/mozilla/geckodriver
 TERMUX_PKG_DESCRIPTION="Proxy for using W3C WebDriver-compatible clients to interact with Gecko-based browsers"
 TERMUX_PKG_LICENSE="MPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
-TERMUX_PKG_VERSION=0.24.0
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_VERSION=0.25.0
 TERMUX_PKG_SRCURL=https://github.com/mozilla/geckodriver/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=e6f86b3b6411f078c0a762f978c00ee99926463036a68be01d111bd91f25340e
+TERMUX_PKG_SHA256=9ba9b1be1a2e47ddd11216ce863903853975a4805e72b9ed5da8bcbcaebbcea9
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_post_make_install() {
