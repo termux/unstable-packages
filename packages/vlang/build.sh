d751353be708668fee44ad3a74d285a445e7261b
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-09-18 21:17:20 +0300

    vlang: update to 0.1.20

--- packages/vlang/build.sh
+++ packages/vlang/build.sh
@@ -2,11 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://github.com/vlang/v
 TERMUX_PKG_DESCRIPTION="Simple, fast, safe, compiled language for developing maintainable software"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
-TERMUX_PKG_VERSION=0.1.18
+TERMUX_PKG_VERSION=0.1.20
 TERMUX_PKG_SRCURL=("https://github.com/vlang/v/archive/$TERMUX_PKG_VERSION.tar.gz"
                    "https://github.com/vlang/vc/archive/$TERMUX_PKG_VERSION.tar.gz")
-TERMUX_PKG_SHA256=("3f3407a78aca7fc3b42a3fc1f1d2b9724c1e4c71fbd5d37ff12976cd2305cec1"
-                   "b8697082b4b89aeadae2a5bf3626459419f3826b061ecf960fc582c854361e2e")
+TERMUX_PKG_SHA256=("8102b48b2c82be6be14633e76e71e215aab5221198315436f97be53e1abe1f5d"
+                   "5b4fc1f39c3aef5214a3366e0d514ee2879a2e52a918dc0181df833028a0eb72")
 TERMUX_PKG_DEPENDS="clang"
 TERMUX_PKG_BUILD_IN_SRC=true
 
@@ -16,7 +16,7 @@ termux_step_make() {
 	(unset CC CFLAGS CPPFLAGS LDFLAGS;
 		gcc "vc-$TERMUX_PKG_VERSION"/v.c -o v.host -lm
 		./v.host -debug -o v.host.2 compiler
-		mv .v.host.2.c v.c
+		mv v.host.2.tmp.c v.c
 	)
 
 	$CC $CFLAGS $CPPFLAGS v.c -o v -lm
