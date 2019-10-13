449ec626b629d1cc947d95eb9a007d0165d17032
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-10-13 22:44:05 +0300

    metasploit: do not package msf sources, let installer script will download them
    That was needed since MSF consumes a lot of our traffic.
    
    %ci:no-build

--- packages/metasploit/build.sh
+++ packages/metasploit/build.sh
@@ -1,11 +1,10 @@
 TERMUX_PKG_HOMEPAGE=https://www.metasploit.com/
-TERMUX_PKG_DESCRIPTION="Advanced open-source platform for developing, testing, and using exploit code"
+TERMUX_PKG_DESCRIPTION="Advanced open-source platform for developing, testing and using exploit code"
 TERMUX_PKG_LICENSE="BSD"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
 TERMUX_PKG_VERSION=5.0.48
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://github.com/rapid7/metasploit-framework/archive/$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=738f8ae0ea1ba7dc8507ea372dcfa02147b7a67f9e2911c288b0d6654f045f79
+TERMUX_PKG_REVISION=2
+TERMUX_PKG_SKIP_SRC_EXTRACT=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
 # Many dependencies specified here are required to build & install
@@ -13,20 +12,12 @@ TERMUX_PKG_PLATFORM_INDEPENDENT=true
 TERMUX_PKG_DEPENDS="apr, apr-util, autoconf, bison, clang, coreutils, curl, findutils, git, libffi, libgmp, libiconv, libpcap, libsqlite, libtool, libxml2, libxslt, make, ncurses, ncurses, ncurses-utils, openssl, pkg-config, postgresql, readline, resolv-conf, ruby, tar, termux-elf-cleaner, termux-tools, unzip, wget, zip, zlib"
 
 termux_step_make_install() {
-	# Metasploit sources.
-	mkdir -p "$TERMUX_PREFIX"/opt
-	rm -rf "$TERMUX_PREFIX"/opt/metasploit-framework
-	cp -a "$TERMUX_PKG_SRCDIR" "$TERMUX_PREFIX"/opt/metasploit-framework
-
 	# Installer.
-	install -Dm700 "$TERMUX_PKG_BUILDER_DIR"/installer.sh \
-		"$TERMUX_PREFIX"/opt/metasploit-framework/installer.sh
-	sed -i "s%\@TERMUX_PREFIX\@%${TERMUX_PREFIX}%g" \
-		"$TERMUX_PREFIX"/opt/metasploit-framework/installer.sh
-
-	# Database configuration file.
-	install -Dm600 "$TERMUX_PKG_BUILDER_DIR"/database.yml \
-		"$TERMUX_PREFIX"/opt/metasploit-framework/config/database.yml
+	sed -e "s|@TERMUX_PREFIX@|${TERMUX_PREFIX}|g" \
+		-e "s|@MSF_VERSION@|$TERMUX_PKG_VERSION|g" \
+		"$TERMUX_PKG_BUILDER_DIR"/installer.sh \
+		> "$TERMUX_PREFIX"/bin/metasploit-installer.sh
+	chmod 700 "$TERMUX_PREFIX"/bin/metasploit-installer.sh
 
 	# Wrapper.
 	install -Dm700 "$TERMUX_PKG_BUILDER_DIR"/msfconsole.sh \
@@ -41,7 +32,7 @@ termux_step_make_install() {
 termux_step_create_debscripts() {
 	{
 		echo "#!$TERMUX_PREFIX/bin/sh"
-		echo "bash $TERMUX_PREFIX/opt/metasploit-framework/installer.sh"
+		echo "bash $TERMUX_PREFIX/bin/metasploit-installer.sh"
 	} > ./postinst
 	chmod 755 ./postinst
 
