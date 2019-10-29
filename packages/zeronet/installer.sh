7d3d195a162fda6f4ed2d2b69922b9574fc8e8c0
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-10-29 16:09:48 +0200

    zeronet: update to 0.7.1

--- packages/zeronet/installer.sh
+++ packages/zeronet/installer.sh
@@ -1,6 +1,5 @@
 #!@TERMUX_PREFIX@/bin/bash
 set -e
-export PREFIX=@TERMUX_PREFIX@
 
 # Lock terminal to prevent sending text input and special key
 # combinations that may break installation process.
@@ -9,24 +8,36 @@ stty -echo -icanon time 0 min 0 intr undef quit undef susp undef
 # Use trap to unlock terminal at exit.
 trap 'while read -r; do true; done; stty sane;' EXIT
 
-# Install 'msgpack' if necessary.
-echo -n "[*] Checking if 'msgpack' is installed: "
-if pip2 show msgpack > /dev/null 2>&1; then
-	echo "yes"
-else
-	echo "no"
-	echo "[*] Installing 'msgpack' for Python 2..."
-	pip2 install msgpack
+# Prevent running apt as root.
+if [ "$(id -u)" = "0" ]; then
+	echo "Installing packages as root ?"
+	echo "Perhaps isn't a good idea... aborting installation :)"
+	exit 1
 fi
 
-# Install 'gevent' if necessary.
-echo -n "[*] Checking if 'gevent' is installed: "
-if pip2 show gevent > /dev/null 2>&1; then
-	echo "yes"
-else
-	echo "no"
-	echo "[*] Installing 'gevent' for Python 2 (may take long time)..."
-	pip2 install gevent
-fi
+# coincurve expects ldconfig to be available.
+mkdir -p "${TMPDIR:-@TERMUX_PREFIX@/tmp}"/.ldconfig
+echo -e "#!@TERMUX_PREFIX@/bin/bash\nexec true" > "${TMPDIR:-@TERMUX_PREFIX@/tmp}"/.ldconfig/ldconfig
+chmod 700 "${TMPDIR:-@TERMUX_PREFIX@/tmp}"/.ldconfig/ldconfig
+export PATH="$PATH:${TMPDIR:-@TERMUX_PREFIX@/tmp}/.ldconfig"
+
+for module in gevent msgpack base58 merkletools rsa PySocks pyasn1 \
+	websocket_client gevent-websocket bencode.py coincurve python-bitcoinlib \
+	maxminddb; do
+
+	echo "Installing Python module: $module..."
+	if ! pip install "$module"; then
+		# Retrying one more time. Useful e.g. when downloading failed.
+		echo "Retrying installation of $module..."
+		if ! pip install "$module"; then
+			# Exit if second attempt failed.
+			echo "Failed to install $module."
+			exit 1
+		fi
+	fi
+done
+
+# Cleanup.
+rm -rf "${TMPDIR:-@TERMUX_PREFIX@/tmp}"/.ldconfig
 
 exit 0
