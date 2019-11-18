a3fb766c31cb5cc326531f8d331e22582bb117af
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-11-18 17:30:55 +0200

    translate-shell: add optional dependencies mpv & hunspell

--- packages/translate-shell/build.sh
+++ packages/translate-shell/build.sh
@@ -7,6 +7,9 @@ TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/soimort/translate-shell/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=589505248212726dff2b3e8828514036491f019fcee8657c0d94bb1a5dac6c5b
 TERMUX_PKG_DEPENDS="bash, curl, gawk, less, rlwrap"
+# hunspell - spell checking
+# mpv - text-to-speech functionality
+TERMUX_PKG_RECOMMENDS="hunspell, mpv"
 TERMUX_PKG_EXTRA_MAKE_ARGS="PREFIX=$TERMUX_PREFIX"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
