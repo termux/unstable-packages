7763ab4b7ec0a6bab22449023467f3b3a7cd6ae2
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-12-22 00:43:32 +0200

    jigdo: update to 0.8.0

--- packages/jigdo/build.sh
+++ packages/jigdo/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=http://atterer.org/jigdo/
 TERMUX_PKG_DESCRIPTION="Distribute large images by sending and receiving the files that make them up"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
-TERMUX_PKG_VERSION=0.7.3
-TERMUX_PKG_REVISION=8
-TERMUX_PKG_SRCURL=http://atterer.org/sites/atterer/files/2009-08/jigdo/jigdo-$TERMUX_PKG_VERSION.tar.bz2
-TERMUX_PKG_SHA256=875c069abad67ce67d032a9479228acdb37c8162236c0e768369505f264827f0
+TERMUX_PKG_VERSION=0.8.0
+TERMUX_PKG_SRCURL=https://www.einval.com/~steve/software/jigdo/download/jigdo-$TERMUX_PKG_VERSION.tar.xz
+TERMUX_PKG_SHA256=f253f72b5719716a7f039877a97ebaf4ba96e877510fca0fb42010d0793db6a4
 TERMUX_PKG_DEPENDS="libbz2, libc++, libdb, wget, zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
 
