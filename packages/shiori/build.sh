f1512bdee1fff7164818e47221a0f146a9de6d84
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-12-22 22:19:19 +0200

    restore package 'shiori'

--- packages/shiori/build.sh
+++ packages/shiori/build.sh
@@ -1,17 +1,16 @@
-TERMUX_PKG_HOMEPAGE=https://github.com/RadhiFadlillah/shiori
+TERMUX_PKG_HOMEPAGE=https://github.com/go-shiori/shiori
 TERMUX_PKG_DESCRIPTION="Simple bookmark manager"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
-TERMUX_PKG_VERSION=1.0
-TERMUX_PKG_REVISION=4
+TERMUX_PKG_VERSION=1.5.0
 TERMUX_PKG_SKIP_SRC_EXTRACT=true
 
 termux_step_make_install() {
 	termux_setup_golang
 	export GOPATH=$TERMUX_PKG_BUILDDIR
 
-	go get -d github.com/RadhiFadlillah/shiori
-	cd "$GOPATH"/src/github.com/RadhiFadlillah/shiori
+	go get -d github.com/go-shiori/shiori
+	cd "$GOPATH"/src/github.com/go-shiori/shiori
 	git checkout "v$TERMUX_PKG_VERSION"
 	git submodule update --init --recursive
 
