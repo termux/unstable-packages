397f85be7aa880198b8d7ec900c4e39bbefc61ab
Leonid Plyushch <leonid.plyushch@gmail.com>
2020-03-01 12:57:05 +0200

    sqlmap: update build.sh

--- packages/sqlmap/build.sh
+++ packages/sqlmap/build.sh
@@ -6,10 +6,10 @@ TERMUX_PKG_VERSION=1.4
 TERMUX_PKG_SRCURL=https://github.com/sqlmapproject/sqlmap/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=a913d177a3ab9e64fc6df1d755e3907323214beceb3a48fae6ea867df1cc434b
 TERMUX_PKG_DEPENDS="python"
+
 termux_step_make_install() {
 	mkdir -p "$PREFIX"/{bin,opt}
 	cp -rf  "$TERMUX_PKG_SRCDIR" "$TERMUX_PREFIX"/opt/sqlmap
-	cd "$TERMUX_PREFIX"/bin
-	ln -sf "$TERMUX_PREFIX"/opt/sqlmap/sqlmap.py sqlmap
-	ln -sf "$TERMUX_PREFIX"/opt/sqlmap/sqlmapapi.py sqlmapapi
+	ln -sfr "$TERMUX_PREFIX"/opt/sqlmap/sqlmap.py "$TERMUX_PREFIX"/bin/sqlmap
+	ln -sfr "$TERMUX_PREFIX"/opt/sqlmap/sqlmapapi.py "$TERMUX_PREFIX"/bin/sqlmapapi
 }
