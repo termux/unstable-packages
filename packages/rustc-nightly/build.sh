8359a66403388a61b2ffcf973472d86cdb96d026
its-pointless <its-pointless@users.noreply.github.com>
2020-03-06 23:07:14 +1100

    rustc-nightly: update to 1.43.0 (#169)

--- packages/rustc-nightly/build.sh
+++ packages/rustc-nightly/build.sh
@@ -3,12 +3,9 @@ TERMUX_PKG_DESCRIPTION="Rust compiler and utilities (nightly version)"
 TERMUX_PKG_DEPENDS="libc++, clang, openssl, lld, zlib, libllvm"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@its-pointless"
-TERMUX_PKG_VERSION=1.41.0
-TERMUX_PKG_REVISION=2
-TERMUX_PKG_SRCURL=https://static.rust-lang.org/dist/2019-12-17/rustc-nightly-src.tar.xz
-TERMUX_PKG_SHA256=1a22149617e3be27570f25354c41d79683e9450db6d529855bd41d0be3e766cb
-#TERMUX_PKG_CONFLICTS="rust-rls-nightly, rust-docs-nightly, rustfmt-nightly"
-#TERMUX_PKG_REPLACES="rust-rls-nightly, rust-docs-nightly, rustfmt-nightly"
+TERMUX_PKG_VERSION=1.43.0
+TERMUX_PKG_SRCURL=https://static.rust-lang.org/dist/2020-03-05/rustc-nightly-src.tar.xz
+TERMUX_PKG_SHA256=aca58a969f12d352e68f3511e72d599e0cd7ef3e639d9fe4da8091fdcba3b205
 TERMUX_PKG_CONFLICTS=rustc-dev-nightly
 TERMUX_PKG_REPLACES=rustc-dev-nightly
 TERMUX_PKG_KEEP_SHARE_DOC=true
@@ -18,11 +15,10 @@ termux_step_configure () {
 	termux_setup_rust
 
 	# nightlys don't build with stable
-	rustup install beta-2019-12-15-x86_64-unknown-linux-gnu
-	rustup default beta-2019-12-15-x86_64-unknown-linux-gnu 
-	rustup component add rustc-dev
+	rustup install beta-2020-02-29-x86_64-unknown-linux-gnu
+#	rustup component add rustc-dev
 	rustup target add $CARGO_TARGET_NAME
-	export  PATH=$HOME/.rustup/toolchains/beta-2019-12-15-x86_64-unknown-linux-gnu/bin:$PATH
+	export  PATH=$HOME/.rustup/toolchains/beta-2020-02-29-x86_64-unknown-linux-gnu/bin:$PATH
 	export	RUST_BACKTRACE=1
 	mkdir -p $TERMUX_PREFIX/opt/rust-nightly
 	RUST_PREFIX=$TERMUX_PREFIX/opt/rust-nightly
@@ -41,9 +37,6 @@ termux_step_configure () {
 	# for backtrace-sys
 	export CC_x86_64_unknown_linux_gnu=gcc
 	export CFLAGS_x86_64_unknown_linux_gnu="-O2"
-	# havent checked if this is needed...
-	sed -i 's/$LDFLAGS/@LDFLAGS@/g'  $PREFIX/bin/llvm-config
-	sed -ie "s|@LDFLAGS@|$LDFLAGS|g"  $PREFIX/bin/llvm-config
 	# it won't link with it in TERMUX_PREFIX/lib without breaking other things.
 	cp $PREFIX/lib/libLLVM-9.0.1.so $TERMUX_STANDALONE_TOOLCHAIN/sysroot/usr/lib/$TERMUX_HOST_PLATFORM/$TERMUX_PKG_API_LEVEL/
 	unset CC CXX CPP LD CFLAGS CXXFLAGS CPPFLAGS LDFLAGS PKG_CONFIG AR RANLIB
@@ -55,7 +48,7 @@ termux_step_configure () {
 }
 
 termux_step_make_install () {
-	../src/x.py dist --host $CARGO_TARGET_NAME --target $CARGO_TARGET_NAME --target wasm32-unknown-unknown || zsh
+	../src/x.py dist --host $CARGO_TARGET_NAME --target $CARGO_TARGET_NAME --target wasm32-unknown-unknown 
 	mkdir $TERMUX_PKG_BUILDDIR/install
 	# g
 	for tar in rustc-nightly rustc-dev-nightly rust-docs-nightly rust-std-nightly rust-analysis-nightly cargo-nightly rls-nightly miri-nightly rustc-dev-nightly rustfmt-nightly clippy-nightly; do
@@ -79,8 +72,6 @@ termux_step_make_install () {
 		rm -f $TERMUX_PREFIX/lib/libc.so  $TERMUX_PREFIX/lib/libdl.so
 		mv $TERMUX_PREFIX/lib/libtinfo.so.6.tmp $TERMUX_PREFIX/lib/libtinfo.so.6
 	fi
-	rm $TERMUX_STANDALONE_TOOLCHAIN/sysroot/usr/lib/$TERMUX_HOST_PLATFORM/$TERMUX_PKG_API_LEVEL/libLLVM-9.0.1.so
-	rm $PREFIX/bin/llvm-config
 	
 }
 
@@ -95,10 +86,12 @@ termux_step_post_massage () {
 	cd $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/lib
 	ln -sf ../opt/rust-nightly/lib/lib*.so .
 	ln -sf $TERMUX_PREFIX/bin/lld $TERMUX_PKG_MASSAGEDIR$RUST_PREFIX/bin/rust-lld
-
+	if [ $TERMUX_ARCH = "x86_64" ]; then
+		rm lib/libtinfo.so.6
+	fi
+	rm	$TERMUX_STANDALONE_TOOLCHAIN/sysroot/usr/lib/$TERMUX_HOST_PLATFORM/$TERMUX_PKG_API_LEVEL/libLLVM-9.0.1.so
 }
 termux_step_create_debscripts () {
-	#echo "Provides: rustc" >> control
 	echo "#!$TERMUX_PREFIX/bin/sh" > postinst
 	echo "echo 'source \$PREFIX/etc/profile.d/rust-nightly.sh to use nightly'" >> postinst
 	echo "echo 'or export RUSTC=\$PREFIX/opt/rust-nightly/bin/rustc'" >> postinst
