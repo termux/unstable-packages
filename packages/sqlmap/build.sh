05a666ce87246720cf827c9be108502c66d8c3b5
Leonid Plyushch <leonid.plyushch@gmail.com>
2020-03-08 22:24:53 +0200

    sqlmap: update to 1.4.3

--- packages/sqlmap/build.sh
+++ packages/sqlmap/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=http://sqlmap.org/
 TERMUX_PKG_DESCRIPTION="Automatic SQL injection and database takeover tool"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Rabby Sheikh @xploitednoob"
-TERMUX_PKG_VERSION=1.4.2
+TERMUX_PKG_VERSION=1.4.3
 TERMUX_PKG_SRCURL=https://github.com/sqlmapproject/sqlmap/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=77faf85164eb17dce769ec830cbd146768644315bc1024613ad13155e09c2d11
+TERMUX_PKG_SHA256=c895b8290cdcb3ac947ffccdda5f3da9726139e85204126fae883022acc79b5c
 TERMUX_PKG_DEPENDS="python"
 
 termux_step_make_install() {
