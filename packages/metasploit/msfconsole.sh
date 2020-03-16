27fe4aa703b02a87908c7cf660170b441ff7c31b
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-03-16 11:01:15 +0200

    metasploit: fix ruby version in wrapper
    https://github.com/termux/termux-packages/issues/5042

--- packages/metasploit/msfconsole.sh
+++ packages/metasploit/msfconsole.sh
@@ -6,16 +6,16 @@ METASPLOIT_PATH="@TERMUX_PREFIX@/opt/metasploit-framework"
 # Fix ruby bigdecimal extensions linking error.
 case "$(uname -m)" in
 	aarch64)
-		export LD_PRELOAD="$LD_PRELOAD:@TERMUX_PREFIX@/lib/ruby/2.6.0/aarch64-linux-android/bigdecimal.so"
+		export LD_PRELOAD="$LD_PRELOAD:@TERMUX_PREFIX@/lib/ruby/2.7.0/aarch64-linux-android/bigdecimal.so"
 		;;
 	arm*)
-		export LD_PRELOAD="$LD_PRELOAD:@TERMUX_PREFIX@/lib/ruby/2.6.0/arm-linux-androideabi/bigdecimal.so"
+		export LD_PRELOAD="$LD_PRELOAD:@TERMUX_PREFIX@/lib/ruby/2.7.0/arm-linux-androideabi/bigdecimal.so"
 		;;
 	i686)
-		export LD_PRELOAD="$LD_PRELOAD:@TERMUX_PREFIX@/lib/ruby/2.6.0/i686-linux-android/bigdecimal.so"
+		export LD_PRELOAD="$LD_PRELOAD:@TERMUX_PREFIX@/lib/ruby/2.7.0/i686-linux-android/bigdecimal.so"
 		;;
 	x86_64)
-		export LD_PRELOAD="$LD_PRELOAD:@TERMUX_PREFIX@/lib/ruby/2.6.0/x86_64-linux-android/bigdecimal.so"
+		export LD_PRELOAD="$LD_PRELOAD:@TERMUX_PREFIX@/lib/ruby/2.7.0/x86_64-linux-android/bigdecimal.so"
 		;;
 	*)
 		;;
