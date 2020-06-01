20db4214d06d1fdd1cf255fd907b437d42052e77
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-06-02 02:53:16 +0300

    libburn: rebuild

--- packages/libburn/build.sh
+++ packages/libburn/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Library for reading, mastering and writing optical discs
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
 TERMUX_PKG_VERSION=1.5.2
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=http://files.libburnia-project.org/releases/libburn-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=7b32db1719d7f6516cce82a9d00dfddfb3581725db732ea87d41ea8ef0ce5227
 TERMUX_PKG_BREAKS="libburn-dev"
