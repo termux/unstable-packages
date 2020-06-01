7691b8bfcb4027e61905e5e1a1591fffa54888fb
Bernd Wachter <bernd-github@wachter.fi>
2020-06-01 13:53:17 +0300

    Add Telegram client library (libtd) (#179)

---
+++ packages/libtd/build.sh
@@ -0,0 +1,15 @@
+TERMUX_PKG_HOMEPAGE=https://core.telegram.org/tdlib/
+TERMUX_PKG_DESCRIPTION="Library for building Telegram clients"
+TERMUX_PKG_LICENSE="BSL-1.0"
+TERMUX_PKG_VERSION=1.6.0
+TERMUX_PKG_REVISION=1
+TERMUX_PKG_SRCURL=https://github.com/tdlib/td/archive/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=9dce57a96f9d4bac8f99aab13ef5cbf6fed04b234a5d22dfa7ef7dce06ea43f8
+TERMUX_PKG_DEPENDS="readline, openssl (>= 1.1.1), zlib"
+TERMUX_PKG_HOSTBUILD=true
+
+termux_step_host_build() {
+        termux_setup_cmake
+        cmake "-DCMAKE_BUILD_TYPE=Release" "$TERMUX_PKG_SRCDIR"
+        cmake --build . --target prepare_cross_compiling
+}
