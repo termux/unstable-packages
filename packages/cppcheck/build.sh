0d7e4127a63048b1840610c9972c8a3797aa44f4
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-06-02 02:52:16 +0300

    cppcheck: rebuild

--- packages/cppcheck/build.sh
+++ packages/cppcheck/build.sh
@@ -1,6 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/danmar/cppcheck
 TERMUX_PKG_DESCRIPTION="tool for static C/C++ code analysis"
 TERMUX_PKG_VERSION=1.90
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/danmar/cppcheck/archive/$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=c4864d3e09359214efdd503b52e241f4f56ba7ce26f8c11939fd9dcfac1fd105
 TERMUX_PKG_LICENSE="GPL-3.0"
