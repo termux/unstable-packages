1d10ebb4dee185d5e8902e04eda934ecfbe6ba53
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-06-21 00:19:09 +0300

    ghc: build for aarch64 only

--- packages/ghc/build.sh
+++ packages/ghc/build.sh
@@ -8,6 +8,7 @@ TERMUX_PKG_DEPENDS="binutils, clang, iconv, libffi, llvm, ncurses"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_NO_STATICSPLIT=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS=" --disable-ld-override --build=x86_64-unknown-linux --host=x86_64-unknown-linux"
+TERMUX_PKG_BLACKLISTED_ARCHES="arm, i686, x86_64"
 
 DYNAMIC_GHC_PROGRAMS=NO
 
