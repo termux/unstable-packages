787cf2c404932f240a822e92677272c850b3ab09
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-06-22 12:15:25 +0300

    fetchmail: update to 6.4.8

--- packages/fetchmail/build.sh
+++ packages/fetchmail/build.sh
@@ -6,9 +6,9 @@ TERMUX_PKG_HOMEPAGE=https://www.fetchmail.info/
 TERMUX_PKG_DESCRIPTION="A remote-mail retrieval utility"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
-TERMUX_PKG_VERSION=6.4.6
+TERMUX_PKG_VERSION=6.4.8
 TERMUX_PKG_SRCURL=https://sourceforge.net/projects/fetchmail/files/branch_${TERMUX_PKG_VERSION:0:3}/fetchmail-$TERMUX_PKG_VERSION.tar.xz
-TERMUX_PKG_SHA256=16efec4b6019b4d2b6e43ed1d4f523dee019ac86754de856d0cf34d503d66011
+TERMUX_PKG_SHA256=26cd936ece146e056cdf79a676a33738b4eab0a5ae2edf3fce5ba034721b09bd
 TERMUX_PKG_DEPENDS="libcrypt, openssl"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-ssl=$TERMUX_PREFIX"
 
