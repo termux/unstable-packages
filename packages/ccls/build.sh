f103075239d50d2b068df9d0869f8c339eea2dd3
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-07-13 17:43:20 +0300

    ccls: create missing files so cmake checks will pass

--- packages/ccls/build.sh
+++ packages/ccls/build.sh
@@ -9,9 +9,9 @@ TERMUX_PKG_DEPENDS="libllvm"
 TERMUX_PKG_BUILD_DEPENDS="rapidjson, libllvm-static"
 
 termux_step_pre_configure() {
-	touch $TERMUX_PREFIX/bin/clang-import-test
+	touch $TERMUX_PREFIX/bin/{clang-import-test,clang-offload-wrapper}
 }
 
 termux_step_post_make_install() {
-	rm $TERMUX_PREFIX/bin/clang-import-test
+	rm $TERMUX_PREFIX/bin/{clang-import-test,clang-offload-wrapper}
 }
