ddfa709ca21e4cb402f316010be015bc36f52c1f
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-08-13 12:27:01 +0000

    cppcheck: bump revision

--- packages/cppcheck/build.sh
+++ packages/cppcheck/build.sh
@@ -1,6 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/danmar/cppcheck
 TERMUX_PKG_DESCRIPTION="tool for static C/C++ code analysis"
 TERMUX_PKG_VERSION=2.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/danmar/cppcheck/archive/$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=447d44bbaa555fa78b89dd2cb0203fd4c6f18269db8a78638b968ba7c72cb02e
 TERMUX_PKG_LICENSE="GPL-3.0"
