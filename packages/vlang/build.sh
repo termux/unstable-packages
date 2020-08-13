cfe6e22490e50dc51640ae27de20aaefee7ccd2b
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-08-13 12:27:26 +0000

    vlang: bump revision

--- packages/vlang/build.sh
+++ packages/vlang/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Simple, fast, safe, compiled language for developing mai
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
 TERMUX_PKG_VERSION=0.1.28
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/vlang/v/archive/$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=0690764f60ceb50dd2ae3fb489c45548a30c569c7ac0e28f9f84c58a643f550f
 TERMUX_PKG_DEPENDS="clang"
