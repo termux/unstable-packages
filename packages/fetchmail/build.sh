c1c1439f770995e837017e56a9cc658d75f2d80a
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-08-13 12:27:05 +0000

    fetchmail: bump revision

--- packages/fetchmail/build.sh
+++ packages/fetchmail/build.sh
@@ -7,6 +7,7 @@ TERMUX_PKG_DESCRIPTION="A remote-mail retrieval utility"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
 TERMUX_PKG_VERSION=6.4.8
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://sourceforge.net/projects/fetchmail/files/branch_${TERMUX_PKG_VERSION:0:3}/fetchmail-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_SHA256=26cd936ece146e056cdf79a676a33738b4eab0a5ae2edf3fce5ba034721b09bd
 TERMUX_PKG_DEPENDS="libcrypt, openssl"
