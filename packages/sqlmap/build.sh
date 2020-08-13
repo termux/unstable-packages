6cd57e5acc2dd27875f4485c58a1885e72ffcedf
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-08-13 12:27:22 +0000

    sqlmap: bump revision

--- packages/sqlmap/build.sh
+++ packages/sqlmap/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Automatic SQL injection and database takeover tool"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Rabby Sheikh @xploitednoob"
 TERMUX_PKG_VERSION=1.4.6
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/sqlmapproject/sqlmap/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=d4ca1b529942c61243759727ec41f43dff19880ed63fca6b10df34f5b71579e6
 TERMUX_PKG_DEPENDS="python"
