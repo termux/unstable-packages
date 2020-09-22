d11bad3f62895ec436f2154c76cd7a463b65b006
Deshdeepak <rkdeshdeepak1@gmail.com>
2020-09-22 10:24:07 +0530

    Update with master

--- packages/telegram-cli/build.sh
+++ packages/telegram-cli/build.sh
@@ -2,9 +2,8 @@ TERMUX_PKG_HOMEPAGE=https://github.com/vysheng/tg
 TERMUX_PKG_DESCRIPTION="Telegram messenger CLI"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
-TERMUX_PKG_VERSION=1:1.3.1-g6547c0b2
-TERMUX_PKG_DEPENDS="libconfig, libevent, libjansson, openssl, readline, zlib"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-liblua"
+TERMUX_PKG_VERSION=1.4.1
+TERMUX_PKG_DEPENDS="libconfig, libevent, libjansson, openssl, readline, zlib, lua"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_SKIP_SRC_EXTRACT=true
 TERMUX_PKG_HOSTBUILD=true
@@ -14,14 +13,13 @@ termux_step_get_source() {
 	cd "$TERMUX_PKG_SRCDIR"
 	git clone https://github.com/vysheng/tg
 	cd tg
-	git checkout 6547c0b21b977b327b3c5e8142963f4bc246187a
 	git submodule update --init --recursive
 	mv * ../
 }
 
 termux_step_host_build() {
 	cp -rf $TERMUX_PKG_SRCDIR/* ./
-	./configure --disable-liblua
+	./configure
 	make bin/generate
 	make bin/tl-parser
 }
