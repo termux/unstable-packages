84ef00840636a99af8e2107205ba9ad50ec7e152
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-10-08 14:25:29 +0300

    haproxy: update build steps
    https://github.com/termux/termux-packages/issues/5906

--- packages/haproxy/build.sh
+++ packages/haproxy/build.sh
@@ -3,29 +3,33 @@ TERMUX_PKG_DESCRIPTION="The Reliable, High Performance TCP/HTTP Load Balancer"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
 TERMUX_PKG_VERSION=2.1.7
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=http://www.haproxy.org/download/${TERMUX_PKG_VERSION:0:3}/src/haproxy-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=392e6cf18e75fe7e166102e8c4512942890a0b5ae738f6064faab4687f60a339
-TERMUX_PKG_DEPENDS="liblua, openssl, pcre, zlib"
+TERMUX_PKG_DEPENDS="liblua53, openssl, pcre, zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
-
-# USE_PCRE=1 requires libpcreposix which is not available. PCRE
-# support will be disabled for now.
-TERMUX_PKG_EXTRA_MAKE_ARGS="
-CPU=generic
-TARGET=generic
-USE_GETADDRINFO=1
-USE_OPENSSL=1
-USE_ZLIB=1
-USE_LUA=1
-LUA_LIB_NAME=lua"
 
 TERMUX_PKG_CONFFILES="etc/haproxy/haproxy.cfg"
 
-termux_step_pre_configure() {
-	if [ $TERMUX_PKG_API_LEVEL -ge 24 ]; then
-		CC="$CC -Wl,-rpath=$TERMUX_PREFIX/lib -Wl,--enable-new-dtags"
-	fi
+termux_step_make() {
+	CC="$CC -Wl,-rpath=$TERMUX_PREFIX/lib -Wl,--enable-new-dtags"
+
+	make \
+		CPU=generic \
+		TARGET=generic \
+		USE_GETADDRINFO=1 \
+		USE_LUA=1 \
+		LUA_INC="$TERMUX_PREFIX/include/lua5.3" \
+		LUA_LIB="$TERMUX_PREFIX/lib"
+		LUA_LIB_NAME=lua5.3 \
+		USE_OPENSSL=1 \
+		USE_PCRE=1 \
+		PCRE_INC="$TERMUX_PREFIX/include" \
+		PCRE_LIB="$TERMUX_PREFIX/lib" \
+		USE_ZLIB=1 \
+		ADDINC="$CPPFLAGS" \
+		CFLAGS="$CFLAGS" \
+		LDFLAGS="$LDFLAGS"
 }
 
 termux_step_post_make_install() {
