b49bc901e6774a80495e486d26e402a8219256ba
Henrik Grimler <henrik@grimler.se>
2020-10-17 23:56:21 +0200

    bftpd: update to 5.6

--- packages/bftpd/build.sh
+++ packages/bftpd/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=http://bftpd.sourceforge.net/
 TERMUX_PKG_DESCRIPTION="Small, easy-to-configure FTP server"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
-TERMUX_PKG_VERSION=5.5
-TERMUX_PKG_REVISION=2
-TERMUX_PKG_SRCURL=https://netix.dl.sourceforge.net/project/bftpd/bftpd/bftpd-${TERMUX_PKG_VERSION/./-}/bftpd-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=1a7aa6026c7f86f0fa2aebafcf3973b84d71324cdef9e33bd492fbf29ca54393
+TERMUX_PKG_VERSION=5.6
+TERMUX_PKG_SRCURL=https://kumisystems.dl.sourceforge.net/project/bftpd/bftpd/bftpd-${TERMUX_PKG_VERSION}/bftpd-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=1094f25afc23c24a1b63db7ebe9f9b72bba4bc3b9245d13d8054ad724ea37aa2
 TERMUX_PKG_DEPENDS="libcrypt"
 TERMUX_PKG_BUILD_IN_SRC=true
 
