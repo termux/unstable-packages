780bff4a9c03f07467417003877d5fa6adb8cba0
Henrik Grimler <henrik@grimler.se>
2020-11-17 21:57:57 +0100

    biber: update to 2.15
    Had to disable tests, those fail on arm (but not aarch64), bug report
    upstream here:
    https://github.com/plk/biber/issues/349

--- packages/biber/build.sh
+++ packages/biber/build.sh
@@ -2,11 +2,10 @@ TERMUX_PKG_HOMEPAGE=http://biblatex-biber.sourceforge.net
 TERMUX_PKG_DESCRIPTION="A Unicode-capable BibTeX replacement for biblatex users"
 TERMUX_PKG_LICENSE="Artistic-License-2.0"
 TERMUX_PKG_MAINTAINER="Henrik Grimler @Grimler91"
-TERMUX_PKG_VERSION=2.14
-TERMUX_PKG_REVISION=4
+TERMUX_PKG_VERSION=2.15
 TERMUX_PKG_SKIP_SRC_EXTRACT=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
-TERMUX_PKG_DEPENDS="perl, libxslt, openssl-tool, make, clang, pkg-config"
+TERMUX_PKG_DEPENDS="perl, libxslt, libxml2 (>> 2.9.10-3), openssl-tool, make, clang, pkg-config"
 
 EXTUTILS_LIBBUILDER_VERSION=0.08
 TEXT_BIBTEX_VERSION=0.88
