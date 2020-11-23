416382567f41dfd76b552a9677c593f000d0fc2d
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-11-23 13:07:55 +0000

    haproxy: update to 2.3.1

--- packages/haproxy/build.sh
+++ packages/haproxy/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.haproxy.org/
 TERMUX_PKG_DESCRIPTION="The Reliable, High Performance TCP/HTTP Load Balancer"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
-TERMUX_PKG_VERSION=2.2.4
+TERMUX_PKG_VERSION=2.3.1
 TERMUX_PKG_SRCURL=http://www.haproxy.org/download/${TERMUX_PKG_VERSION:0:3}/src/haproxy-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=87a4d9d4ff8dc3094cb61bbed4a8eed2c40b5ac47b9604daebaf036d7b541be2
+TERMUX_PKG_SHA256=8d3bf1252a5b60b21e9885c8d0d6d89e932d320c2977a6522aed6df81eefca4b
 TERMUX_PKG_DEPENDS="liblua53, openssl, pcre, zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
 
