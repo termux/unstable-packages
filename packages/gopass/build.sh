bb336155e42ec232292bc6a7215fc50c8c17f31c
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-02-08 19:02:26 +0000

    gopass: update to 1.11.0

--- packages/gopass/build.sh
+++ packages/gopass/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/gopasspw/gopass
 TERMUX_PKG_DESCRIPTION="The slightly more awesome standard unix password manager for teams."
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
-TERMUX_PKG_VERSION=1.10.1
+TERMUX_PKG_VERSION=1.11.0
 TERMUX_PKG_SRCURL=https://github.com/gopasspw/gopass/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=f4bfe03bc26701baf411795faafe774aa6025bc6050ab3ebf7e931e5ff821961
+TERMUX_PKG_SHA256=26ac90ef60d7f08e7d9361280d6457edb90642e821edba2c5e5cb0a09ca20b08
 TERMUX_PKG_DEPENDS="git, gnupg"
 TERMUX_PKG_SUGGESTS="termux-api"
 
