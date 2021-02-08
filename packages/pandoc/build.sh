b8401b9e127857c8a18321fd9d2b6e2d50130cc0
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-02-08 19:03:17 +0000

    pandoc: update to 2.11.4

--- packages/pandoc/build.sh
+++ packages/pandoc/build.sh
@@ -4,28 +4,22 @@ TERMUX_PKG_HOMEPAGE=https://pandoc.org/
 TERMUX_PKG_DESCRIPTION="Universal markup converter"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
-TERMUX_PKG_VERSION=2.9.2.1
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=2.11.4
 TERMUX_PKG_SRCURL=https://github.com/jgm/pandoc/releases/download/$TERMUX_PKG_VERSION/pandoc-${TERMUX_PKG_VERSION}-linux-amd64.tar.gz
-TERMUX_PKG_SHA256=5b61a981bd2b7d48c1b4ba5788f1386631f97e2b46d0d1f1a08787091b4b0cf8
+TERMUX_PKG_SHA256=b15ce6009ab833fb51fc472bf8bb9683cd2bd7f8ac948f3ddeb6b8f9a366d69a
 TERMUX_PKG_DEPENDS="qemu-user-x86_64"
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make_install() {
-	local file
-	for file in pandoc pandoc-citeproc; do
-		sed \
-			-e "s|@TERMUX_PREFIX@|$TERMUX_PREFIX|g" \
-			-e "s|@BINARY@|$file|g" \
-			"$TERMUX_PKG_BUILDER_DIR/wrapper.sh.in" \
-				> "$TERMUX_PREFIX/bin/$file"
-
-		chmod 700 "$TERMUX_PREFIX/bin/$file"
-
-		install -Dm700 "./bin/$file" "$TERMUX_PREFIX/libexec/pandoc/$file"
-		install -Dm600 "./share/man/man1/$file.1.gz" "$TERMUX_PREFIX/share/man/man1/$file.1.gz"
-	done
+	sed \
+		-e "s|@TERMUX_PREFIX@|$TERMUX_PREFIX|g" \
+		-e "s|@BINARY@|pandoc|g" \
+		"$TERMUX_PKG_BUILDER_DIR/wrapper.sh.in" \
+			> "$TERMUX_PREFIX/bin/pandoc"
+	chmod 700 "$TERMUX_PREFIX/bin/pandoc"
+	install -Dm700 "./bin/pandoc" "$TERMUX_PREFIX/libexec/pandoc/pandoc"
+	install -Dm600 "./share/man/man1/pandoc.1.gz" "$TERMUX_PREFIX/share/man/man1/pandoc.1.gz"
 }
 
 termux_step_create_debscripts() {
