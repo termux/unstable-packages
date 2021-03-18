c17d863a4c108ab2d0fb54a3a70bc5860b3b5202
Dominik Schulz <mail@dschulz.eu>
2021-03-18 20:52:48 +0100

    gopass: update to 1.12.2
    Signed-off-by: Dominik Schulz <dominik.schulz@gauner.org>

--- packages/gopass/build.sh
+++ packages/gopass/build.sh
@@ -2,10 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://github.com/gopasspw/gopass
 TERMUX_PKG_DESCRIPTION="The slightly more awesome standard unix password manager for teams."
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
-TERMUX_PKG_VERSION=1.11.0
+TERMUX_PKG_VERSION=1.12.2
 TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/gopasspw/gopass/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=26ac90ef60d7f08e7d9361280d6457edb90642e821edba2c5e5cb0a09ca20b08
+TERMUX_PKG_SHA256=a7032352192aed91ee9bd827fa4aa57644562fecf4aa9a8cb4de69f786e9750d
 TERMUX_PKG_DEPENDS="git, gnupg"
 TERMUX_PKG_SUGGESTS="termux-api"
 
@@ -16,6 +16,9 @@ termux_step_make() {
 	mkdir -p ./src
 	mkdir -p ./src/github.com/gopasspw
 	ln -sf "$TERMUX_PKG_SRCDIR" ./src/github.com/gopasspw/gopass
+
+	# Generate the man page on the host or make install will fail.
+	cd src/github.com/gopasspw/gopass && GOOS=linux GOARCH=amd64 go run helpers/man/main.go > gopass.1
 
 	# Build gopass for host so we can generate completion for shells.
 	GOOS=linux GOARCH=amd64 CC=gcc LD=gcc make -C ./src/github.com/gopasspw/gopass build completion
