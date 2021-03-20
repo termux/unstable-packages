7bf93b0ff60be7dc5fcde27b3c70a3d9b040d1cf
Dominik Schulz <mail@dschulz.eu>
2021-03-20 13:53:42 +0100

    gopass: update to 1.12.4
    Signed-off-by: Dominik Schulz <dominik.schulz@gauner.org>

--- packages/gopass/build.sh
+++ packages/gopass/build.sh
@@ -2,10 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://github.com/gopasspw/gopass
 TERMUX_PKG_DESCRIPTION="The slightly more awesome standard unix password manager for teams."
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
-TERMUX_PKG_VERSION=1.11.0
+TERMUX_PKG_VERSION=1.12.4
 TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/gopasspw/gopass/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=26ac90ef60d7f08e7d9361280d6457edb90642e821edba2c5e5cb0a09ca20b08
+TERMUX_PKG_SHA256=1e3d7ad389a8462256ff7e439de1fbed8012c38ec33bded63b11af74f0525279
 TERMUX_PKG_DEPENDS="git, gnupg"
 TERMUX_PKG_SUGGESTS="termux-api"
 
@@ -17,13 +17,8 @@ termux_step_make() {
 	mkdir -p ./src/github.com/gopasspw
 	ln -sf "$TERMUX_PKG_SRCDIR" ./src/github.com/gopasspw/gopass
 
-	# Build gopass for host so we can generate completion for shells.
-	GOOS=linux GOARCH=amd64 CC=gcc LD=gcc make -C ./src/github.com/gopasspw/gopass build completion
-	make -C ./src/github.com/gopasspw/gopass install PREFIX="$TERMUX_PREFIX"
-
-	# Finally build gopass for target.
 	rm -f ./src/github.com/gopasspw/gopass/gopass
-	make -C ./src/github.com/gopasspw/gopass build
+	make -C ./src/github.com/gopasspw/gopass build CLIPHELPERS="-X github.com/gopasspw/gopass/pkg/clipboard.Helpers=termux-api'"
 	install -Dm700 \
 		./src/github.com/gopasspw/gopass/gopass \
 		"$TERMUX_PREFIX"/bin/gopass
